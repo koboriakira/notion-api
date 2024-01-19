6b37f88bbee02bbefb3a25e43085b2e9dfcc4f9d
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-01-19 20:34:30 +0900

    Refactor CDK stack and add Notion API

---
+++ cdk/lib/notion-api.ts
@@ -0,0 +1,94 @@
+/** @format */
+
+import {
+  Stack,
+  StackProps,
+  Duration,
+  aws_lambda as lambda,
+  aws_iam as iam,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+// CONFIG
+const RUNTIME = lambda.Runtime.PYTHON_3_11;
+const TIMEOUT = 30;
+const APP_DIR_PATH = "../notion_api";
+const HANDLER_NAME = "main.handler";
+const LAYER_ZIP_PATH = "../dependencies.zip";
+
+export class NotionApi extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const role = this.makeRole();
+    const myLayer = this.makeLayer();
+    /*const fn = */ this.createLambdaFunction(role, myLayer);
+  }
+
+  /**
+   * Create or retrieve an IAM role for the Lambda function.
+   * @returns {iam.Role} The created or retrieved IAM role.
+   */
+  makeRole() {
+    // Lambdaの実行ロールを取得または新規作成
+    const role = new iam.Role(this, "LambdaRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    });
+
+    // Lambda の実行ロールに管理ポリシーを追加
+    role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AWSLambdaBasicExecutionRole"
+      )
+    );
+
+    // 必要に応じて追加の権限をポリシーとしてロールに付与
+    role.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        actions: ["lambda:InvokeFunction", "lambda:InvokeAsync"],
+        resources: ["*"],
+      })
+    );
+
+    return role;
+  }
+
+  /**
+   * Create or retrieve a Lambda layer.
+   * @returns {lambda.LayerVersion} The created or retrieved Lambda layer.
+   */
+  makeLayer() {
+    return new lambda.LayerVersion(this, "Layer", {
+      code: lambda.Code.fromAsset(LAYER_ZIP_PATH), // レイヤーの内容を含むディレクトリ
+      compatibleRuntimes: [RUNTIME], // このレイヤーが互換性を持つランタイム
+    });
+  }
+
+  /**
+   * Create a Lambda function.
+   * @param {iam.Role} role The IAM role for the Lambda function.
+   * @param {lambda.LayerVersion} myLayer The Lambda layer to be used.
+   * @returns {lambda.Function} The created Lambda function.
+   */
+  createLambdaFunction(
+    role: iam.Role,
+    myLayer: lambda.LayerVersion
+  ): lambda.Function {
+    const fn = new lambda.Function(this, "Lambda", {
+      runtime: RUNTIME,
+      handler: HANDLER_NAME,
+      code: lambda.Code.fromAsset(APP_DIR_PATH),
+      role: role,
+      layers: [myLayer],
+      timeout: Duration.seconds(TIMEOUT),
+    });
+
+    fn.addEnvironment("NOTION_SECRET", process.env.NOTION_SECRET || "");
+
+    fn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE, // 認証なし
+    });
+
+    return fn;
+  }
+}
