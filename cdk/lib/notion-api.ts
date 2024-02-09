1aec7a5c4e3d2d476dec8faed02f0d9486e6ba9e
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-02-09 14:47:50 +0900

    Add scheduler and convert function name to camel case

--- cdk/lib/notion-api.ts
+++ cdk/lib/notion-api.ts
@@ -20,6 +20,67 @@ const TIMEOUT = 30;
 const APP_DIR_PATH = "../notion_api";
 const LAYER_ZIP_PATH = "../dependencies.zip";
 
+// SHCEDULER
+
+// 月曜10時に実行
+const CRON_SCHEDULE_CREATE_DAILY_LOG = events.Schedule.cron({
+  minute: "0",
+  hour: "1",
+  month: "*",
+  year: "*",
+  weekDay: "MON",
+});
+
+// 10分ごとに実行
+const CRON_SCHEDULE_CLEAN_EMPTY_TITLE_PAG = events.Schedule.cron({
+  minute: "*/10",
+  hour: "*",
+  month: "*",
+  year: "*",
+  weekDay: "*",
+});
+
+// 毎日22時に実行
+const CRON_SCHEDULE_COLLECT_UPDATED_PAGES = events.Schedule.cron({
+  minute: "0",
+  hour: "13",
+  month: "*",
+  year: "*",
+  weekDay: "*",
+});
+
+// 毎日2時に実行
+const CRON_SCHEDULE_POSTPONE_TASK = events.Schedule.cron({
+  minute: "0",
+  hour: "17",
+  month: "*",
+  year: "*",
+  weekDay: "*",
+});
+
+// 3分ごとに実行(8時から24時まで)
+const CRON_SCHEDULE_UPDATE_CURRENT_TASKS = events.Schedule.cron({
+  minute: "*/3",
+  hour: "23-15",
+  month: "*",
+  year: "*",
+  weekDay: "*",
+});
+
+/**
+ * Convert a string to camel case.
+ * @param text
+ * @returns
+ */
+const convertToCamelCase = (text: string): string => {
+  return text
+    .split("_")
+    .map((word, index) =>
+      index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)
+    )
+    .join("");
+};
+
 export class NotionApi extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -32,82 +93,42 @@ export class NotionApi extends Stack {
 
     const role = this.makeRole(bucket.bucketArn);
     const myLayer = this.makeLayer();
-    const fn = this.createLambdaFunction(
-      "Lambda",
-      "main.handler",
-      role,
-      myLayer
-    );
-    this.makeApiGateway(fn);
+    const main = this.createLambdaFunction("main", role, myLayer);
+    this.makeApiGateway(main);
 
     this.createEventLambda(
       "create_daily_log",
       role,
       myLayer,
-      // 毎週月曜日10:00に実行
-      events.Schedule.cron({
-        minute: "0",
-        hour: "1",
-        month: "*",
-        year: "*",
-        weekDay: "MON",
-      })
+      CRON_SCHEDULE_CREATE_DAILY_LOG
     );
 
     this.createEventLambda(
       "clean_empty_title_page",
       role,
       myLayer,
-      // 10分ごとに実行
-      events.Schedule.cron({
-        minute: "*/10",
-        hour: "*",
-        month: "*",
-        year: "*",
-        weekDay: "*",
-      })
+      CRON_SCHEDULE_CLEAN_EMPTY_TITLE_PAG
     );
 
     this.createEventLambda(
       "collect_updated_pages",
       role,
       myLayer,
-      // 毎日22時に実行
-      events.Schedule.cron({
-        minute: "0",
-        hour: "13",
-        month: "*",
-        year: "*",
-        weekDay: "*",
-      })
+      CRON_SCHEDULE_COLLECT_UPDATED_PAGES
     );
 
     this.createEventLambda(
       "postpone_task",
       role,
       myLayer,
-      // 毎日2時に実行
-      events.Schedule.cron({
-        minute: "0",
-        hour: "17",
-        month: "*",
-        year: "*",
-        weekDay: "*",
-      })
+      CRON_SCHEDULE_POSTPONE_TASK
     );
 
     this.createEventLambda(
       "update_current_tasks",
       role,
       myLayer,
-      // 3分ごとに実行(8時から24時まで)
-      events.Schedule.cron({
-        minute: "*/3",
-        hour: "23-15",
-        month: "*",
-        year: "*",
-        weekDay: "*",
-      })
+      CRON_SCHEDULE_UPDATE_CURRENT_TASKS
     );
   }
 
@@ -117,22 +138,8 @@ export class NotionApi extends Stack {
     myLayer: lambda.LayerVersion,
     schedule: events.Schedule
   ): lambda.Function {
-    // スネークケースであるhandler_nameをキャメルケースに変換
-    const resourceName = handlerName
-      .split("_")
-      .map((word, index) =>
-        index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)
-      )
-      .join("");
-
-    const fn = this.createLambdaFunction(
-      resourceName,
-      `${handlerName}.handler`,
-      role,
-      myLayer,
-      false
-    );
-    new events.Rule(this, `${resourceName}Rule`, {
+    const fn = this.createLambdaFunction(handlerName, role, myLayer, false);
+    new events.Rule(this, `${convertToCamelCase(handlerName)}Rule`, {
       schedule: schedule,
       targets: [new targets.LambdaFunction(fn, { retryAttempts: 0 })],
     });
@@ -191,15 +198,16 @@ export class NotionApi extends Stack {
    * @returns {lambda.Function} The created Lambda function.
    */
   createLambdaFunction(
-    name: string,
-    handler_name: string,
+    handlerName: string,
     role: iam.Role,
     myLayer: lambda.LayerVersion,
     function_url_enabled: boolean = false
   ): lambda.Function {
-    const fn = new lambda.Function(this, name, {
+    const resourceNameCamel = convertToCamelCase(handlerName);
+
+    const fn = new lambda.Function(this, resourceNameCamel, {
       runtime: RUNTIME,
-      handler: handler_name,
+      handler: handlerName + ".handler",
       code: lambda.Code.fromAsset(APP_DIR_PATH),
       role: role,
       layers: [myLayer],
