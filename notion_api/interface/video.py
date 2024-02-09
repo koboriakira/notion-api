a39893bdb2e0a92db0780e1f3006cd12d26b0be1
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-02-09 17:00:58 +0900

    Add ErrorReporter service for handling exceptions

--- notion_api/interface/video.py
+++ notion_api/interface/video.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from custom_logger import get_logger
 from usecase.add_video_usecase import AddVideoUsecase
+from usecase.service.error_reporter import ErrorReporter
 
 logger = get_logger(__name__)
 
@@ -12,21 +13,28 @@ def add_page(
         slack_channel: Optional[str] = None,
         slack_thread_ts: Optional[str] = None,
             ) -> dict:
-    logger.debug(f"url: {url}")
-    logger.debug(f"title: {title}")
-    logger.debug(f"tags: {tags}")
-    logger.debug(f"cover: {cover}")
-    logger.debug(f"slack_channel: {slack_channel}")
-    logger.debug(f"slack_thread_ts: {slack_thread_ts}")
+    try:
+        logger.debug(f"url: {url}")
+        logger.debug(f"title: {title}")
+        logger.debug(f"tags: {tags}")
+        logger.debug(f"cover: {cover}")
+        logger.debug(f"slack_channel: {slack_channel}")
+        logger.debug(f"slack_thread_ts: {slack_thread_ts}")
 
-    usecase = AddVideoUsecase()
-    result = usecase.execute(
-        url=url,
-        title=title,
-        tags=tags,
-        cover=cover,
-        slack_channel=slack_channel,
-        slack_thread_ts=slack_thread_ts,
+        usecase = AddVideoUsecase()
+        result = usecase.execute(
+            url=url,
+            title=title,
+            tags=tags,
+            cover=cover,
+            slack_channel=slack_channel,
+            slack_thread_ts=slack_thread_ts,
+            )
+        logger.debug(result)
+        return result
+    except Exception as e:
+        ErrorReporter().report_error(
+            err=e,
+            channel=slack_channel,
+            thread_ts=slack_thread_ts,
         )
-    logger.debug(result)
-    return result
