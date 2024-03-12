a35070cd69e1acf13b929a9362cb169eb99ddcd0
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-12 10:21:42 +0900

    notion_api/create_page.pyの変更を追加

--- notion_api/create_page.py
+++ notion_api/create_page.py
@@ -2,6 +2,7 @@ import logging
 import json
 from util.environment import Environment
 from interface import video, webclip
+from infrastructure.slack_bot_client import SlackBotClient
 
 # ログ
 logging.basicConfig(level=logging.INFO)
@@ -9,14 +10,22 @@ if Environment.is_dev():
     logging.basicConfig(level=logging.DEBUG)
 
 
+def find_request(event: dict) -> dict:
+    try:
+        return json.loads(event["Records"][0]["body"])
+    except Exception as e:
+        print("event", event)
+        print("records", event["Records"])
+        raise
+
+
 def handler(event, context):
-    print("event", event)
-    print("records", event["Records"])
     request: dict = json.loads(event["Records"][0]["body"])
     print("request", request)
+    mode: str = request["mode"]
     params: dict = request["params"]
     try:
-        if request["mode"] == "video":
+        if mode == "video":
             video.add_page(
                 url=params["url"],
                 title=params["title"],
@@ -24,7 +33,7 @@ def handler(event, context):
                 slack_channel=params.get("slack_channel") or None,
                 slack_thread_ts=params.get("slack_thread_ts") or None,
             )
-        if request["mode"] == "webclip":
+        if mode == "webclip":
             webclip.add_page(
                 url=params["url"],
                 title=params["title"],
@@ -33,9 +42,6 @@ def handler(event, context):
                 slack_thread_ts=params.get("slack_thread_ts") or None,
             )
     except Exception as e:
-        from infrastructure.slack_bot_client import SlackBotClient
-
-        # SlackにRateLimitErrorを通知する
         slack_bot_client = SlackBotClient()
         error_message = f"エラーが発生しました: {e}"
         slack_bot_client.send_message(channel="C04Q3AV4TA5", text=error_message)
