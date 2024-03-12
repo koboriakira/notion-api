7d83cfbb2a635946bc7f25782612294c2472571a
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-12 10:12:37 +0900

    SlackBotClientのsend_messageメソッドにメンション機能を追加

--- notion_api/infrastructure/slack_bot_client.py
+++ notion_api/infrastructure/slack_bot_client.py
@@ -3,15 +3,29 @@ from slack_sdk import WebClient
 import os
 from domain.infrastructure.slack_client import SlackClient
 
+
 class SlackBotClient(SlackClient):
     def __init__(self):
         self.client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
 
-    def send_message(self, channel: str, text: str, blocks: Optional[list] = None, thread_ts: Optional[str] = None) -> dict:
-        return self.client.chat_postMessage(channel=channel, text=text, blocks=blocks, thread_ts=thread_ts)
+    def send_message(
+        self,
+        channel: str,
+        text: str,
+        blocks: Optional[list] = None,
+        thread_ts: Optional[str] = None,
+        is_enabled_mention: bool = False,
+    ) -> dict:
+        text = f"<@U04PQMBCFNE> {text}" if is_enabled_mention else text
+        return self.client.chat_postMessage(
+            channel=channel, text=text, blocks=blocks, thread_ts=thread_ts
+        )
 
-    def update_message(self, channel: str, ts: str, text: str, blocks: Optional[list] = None) -> dict:
+    def update_message(
+        self, channel: str, ts: str, text: str, blocks: Optional[list] = None
+    ) -> dict:
         return self.client.chat_update(channel=channel, ts=ts, text=text, blocks=blocks)
+
 
 TEST = "C05H3USHAJU"
 
--- notion_api/usecase/service/openai_executer.py
+++ notion_api/usecase/service/openai_executer.py
@@ -1,24 +1,30 @@
 from openai import OpenAI
+from openai.error import RateLimitError
 from typing import Optional, Callable, Any
 import logging
 import json
 
 OPENAI_MODEL_DEFAULT = "gpt-3.5-turbo-1106"
 
+
 class OpenaiExecuter:
-    def __init__(self, model: str = OPENAI_MODEL_DEFAULT, logger: Optional[logging.Logger] = None):
+    def __init__(
+        self, model: str = OPENAI_MODEL_DEFAULT, logger: Optional[logging.Logger] = None
+    ):
         self.model = model
         self.logger = logger or logging.getLogger(__name__)
         self.client = OpenAI()
 
     def simple_chat(self, user_content: str) -> str:
-        """ メッセージをOpenAIに送信して、返答を受け取る """
+        """メッセージをOpenAIに送信して、返答を受け取る"""
         messages = [{"role": "user", "content": user_content}]
         response_message = self.__chat_completions_create(messages=messages)
         self.logger.debug(response_message)
         return response_message.content
 
-    def simple_function_calling(self, user_content: str, func: Callable, func_description: str, parameters: dict) -> Any:
+    def simple_function_calling(
+        self, user_content: str, func: Callable, func_description: str, parameters: dict
+    ) -> Any:
         """
         シンプルな単一のfunction callingを実行して、実行結果を受け取る
 
@@ -35,16 +41,20 @@ class OpenaiExecuter:
                     "name": func.__name__,
                     "description": func_description,
                     "parameters": parameters,
-                }
+                },
             }
         ]
-        response_message = self.__chat_completions_create(messages=messages, tools=tools, tool_choice="auto")
+        response_message = self.__chat_completions_create(
+            messages=messages, tools=tools, tool_choice="auto"
+        )
         tool_calls = response_message.tool_calls
         if not tool_calls or len(tool_calls) == 0:
             self.logger.warning("tool_calls is empty")
             return None
 
-        available_functions = {func.__name__: func,}
+        available_functions = {
+            func.__name__: func,
+        }
         for tool_call in tool_calls:
             function_name = tool_call.function.name
             function_to_call = available_functions[function_name]
@@ -53,23 +63,45 @@ class OpenaiExecuter:
             return function_response
         raise Exception("tool_call is not found")
 
-    def __chat_completions_create(self, messages: list[dict], tools: Optional[list[dict]] = None, tool_choice: Optional[str] = None):
-        """ OpenAIのchat_completions.createを呼び出す """
-        if tools is None or tool_choice is None:
-            response = self.client.chat.completions.create(
-              model=self.model,
-              messages=messages,
+    def __chat_completions_create(
+        self,
+        messages: list[dict],
+        tools: Optional[list[dict]] = None,
+        tool_choice: Optional[str] = None,
+    ):
+        """OpenAIのchat_completions.createを呼び出す"""
+        try:
+            if tools is None or tool_choice is None:
+                response = self.client.chat.completions.create(
+                    model=self.model,
+                    messages=messages,
+                )
+                self.logger.debug(response)
+                response_message = response.choices[0].message
+                return response_message
+            else:
+                response = self.client.chat.completions.create(
+                    model=self.model,
+                    messages=messages,
+                    tools=tools,
+                    tool_choice="auto",
+                )
+                self.logger.debug(response)
+                response_message = response.choices[0].message
+                return response_message
+        except RateLimitError as e:
+            from infrastructure.slack_bot_client import SlackBotClient
+
+            # SlackにRateLimitErrorを通知する
+            slack_bot_client = SlackBotClient()
+            slack_bot_client.send_message(
+                channel="C04Q3AV4TA5", text="OpenAIのRateLimitErrorが発生しました。"
             )
-            self.logger.debug(response)
-            response_message = response.choices[0].message
-            return response_message
-        else:
-            response = self.client.chat.completions.create(
-                model=self.model,
-                messages=messages,
-                tools=tools,
-                tool_choice="auto",
+            slack_bot_client.send_message(
+                channel="C04Q3AV4TA5",
+                text="OpenAIのRateLimitErrorが発生しました。",
+                is_enabled_mention=True,
             )
-            self.logger.debug(response)
-            response_message = response.choices[0].message
-            return response_message
+            raise e
+        except Exception as e:
+            raise e
