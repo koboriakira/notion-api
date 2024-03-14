fe9a96e84423fe0d9d4f062ee4526f4ef6fd5310
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-14 17:07:41 +0900

    フォーマット

--- notion_api/notion_client_wrapper/properties/property.py
+++ notion_api/notion_client_wrapper/properties/property.py
@@ -1,11 +1,15 @@
 from abc import ABCMeta, abstractmethod
-from typing import Optional, Any
+
 
 class Property(metaclass=ABCMeta):
-    id: Optional[str]
+    id: str | None
     name: str
     type: str
 
     @abstractmethod
     def __dict__(self):
         pass
+
+    # @abstractmethod
+    # def value_for_filter(self) -> str:
+    #     pass
