fddd2832c9032e51090eadda80341ae395e608be
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-15 01:43:46 +0900

    FetchTasksUsecaseのタスク検索をリファクタリングする

--- notion_api/usecase/fetch_tasks_usecase.py
+++ notion_api/usecase/fetch_tasks_usecase.py
@@ -1,55 +1,23 @@
-from datetime import date, datetime
+from datetime import date
 
 from custom_logger import get_logger
-from domain.database_type import DatabaseType
-from domain.task import TaskStatus
-from notion_client_wrapper.client_wrapper import ClientWrapper
+from domain.task.task_repository import TaskRepository, TaskRepositoryImpl
 from usecase.service.base_page_converter import BasePageConverter
 from util.datetime import jst_today
 
 logger = get_logger(__name__)
 
 class FetchTasksUsecase:
-    def __init__(self, notion_client_wrapper: ClientWrapper|None=None) -> None:
-        self.client = notion_client_wrapper or ClientWrapper.get_instance()
+    def __init__(self, task_repository: TaskRepository|None=None) -> None:
+        self.task_repository = task_repository or TaskRepositoryImpl()
 
     def execute(
             self,
             status_list: list[str],
             start_date: date | None = None) -> list[dict]:
-        # まず全てのタスクを集める
-        all_pages = self.client.retrieve_database(database_id=DatabaseType.TASK.value)
-        all_tasks = [BasePageConverter.to_task(p) for p in all_pages]
-
-        # ステータス条件を取得
-        status_cond_list = TaskStatus.get_status_list(status_list)
-        status_cond_name_list = [s.value for s in status_cond_list]
-        logger.debug(f"status_cond_name_list: {status_cond_name_list}")
-
-        tasks = []
-        for task in all_tasks:
-            # タスク種別:ゴミ箱は除外する
-            if task.get("task_kind") == "ゴミ箱":
-                continue
-
-            # 実施日が指定されている場合は、実施日が一致するもののみを返す
-            if start_date is not None:
-                if task["start_date"] is None:
-                    continue
-                task_start_date = _convert_to_date(task["start_date"])
-                if task_start_date != start_date:
-                    continue
-
-            # ステータスが指定されている場合は、ステータスが一致するもののみを返す
-            if len(status_cond_list) > 0 and task["status"] not in status_cond_name_list:
-                continue
-            tasks.append(task)
-        return tasks
+        tasks = self.task_repository.search(status_list=status_list, start_date=start_date)
+        # FIXME: このままTaskを返すようにリファクタリングする
+        return [BasePageConverter.to_task(t) for t in tasks]
 
     def current(self) -> list[dict]:
         return self.execute(status_list=["ToDo", "InProgress"], start_date=jst_today())
-
-def _convert_to_date(value: str) -> date:
-    if len(value) == 10:
-        return date.fromisoformat(value)
-    return datetime.fromisoformat(value).date()
