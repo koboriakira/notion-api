d25c892235142f869e6b6923a7a6384070af9282
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-19 01:57:06 +0900

    日次ログ作成と更新ページ収集の関数を修正

--- notion_api/interface/daily_log.py
+++ notion_api/interface/daily_log.py
@@ -1,20 +1,20 @@
-from datetime import date as Date
-from typing import Optional
+from datetime import date, datetime
+
 from custom_logger import get_logger
-from usecase.create_daily_log_usecase import CreateDailyLogUsecase
 from usecase.collect_updated_pages_usecase import CollectUpdatedPagesUsecase
+from usecase.create_daily_log_usecase import CreateDailyLogUsecase
+from util.datetime import jst_now
 
 logger = get_logger(__name__)
 
-def create_daily_log(target_date: Date = Date.today()):
+def create_daily_log(target_date: date|None = None) -> None:
+    target_date = target_date or jst_now()
     usecase = CreateDailyLogUsecase()
-    usecase.handle(year=target_date.year, isoweeknum=target_date.isocalendar()[1])
+    usecase.handle(
+        year=target_date.year,
+        isoweeknum=target_date.isocalendar()[1],
+        )
 
-def collect_updated_pages(date: Optional[Date] = None):
+def collect_updated_pages(target_datetime: datetime | None = None) -> None:
     usecase = CollectUpdatedPagesUsecase()
-    usecase.execute(date)
-
-if __name__ == "__main__":
-    # python -m interface.daily_log
-    # create_daily_log(target_date=Date(2024, 1, 29))
-    collect_updated_pages(date=Date(2024, 1, 29))
+    usecase.execute(target_datetime=target_datetime)
