4de60cdb97eed49408184376830df05b1ffe44d3
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-20 21:50:14 +0900

    タスクの検索関数の引数の整形

--- notion_api/router/task.py
+++ notion_api/router/task.py
@@ -13,8 +13,9 @@ router = APIRouter()
 
 
 @router.get("/{task_id}", response_model=TaskResponse)
-def find_task(task_id: str,
-              access_token: str | None = Header(None)):
+def find_task(
+        task_id: str,
+        access_token: str | None = Header(None)) -> TaskResponse:
     valid_access_token(access_token)
     task_result = task.find_task(id=task_id)
     return TaskResponse(data=Task.from_params(task_result))
