70c0626553f361313ed830cc1467191793269ffd
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-20 14:40:20 +0900

    ルーティンタスクの日付計算ロジックを追加

--- notion_api/domain/task/routine_task.py
+++ notion_api/domain/task/routine_task.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
+from datetime import date, timedelta
 from enum import Enum
+
+from util.datetime import jst_today
 
 
 class RoutineType(Enum):
@@ -10,6 +13,29 @@ class RoutineType(Enum):
     DAYS_AFTER_7 = "7日後"
     DAYS_AFTER_3 = "3日後"
 
+
+    def next_date(self, basis_date: date) -> date:  # noqa: PLR0911
+        weekday = basis_date.weekday()
+        match self:
+            case RoutineType.DAILY:
+                return basis_date
+            case RoutineType.EVERY_SAT:
+                return basis_date + timedelta(days=(5 - weekday) % 7)
+            case RoutineType.EVERY_TUE_AND_FRI:
+                if weekday in [1, 4]:
+                    return basis_date
+                if weekday in [2, 3]:
+                    return basis_date + timedelta(days=(4 - weekday) % 7)
+                return basis_date + timedelta(days=(1 - weekday) % 7)
+            case RoutineType.EVERY_WED:
+                return basis_date + timedelta(days=(2 - weekday) % 7)
+            case RoutineType.DAYS_AFTER_7:
+                return basis_date + timedelta(days=7)
+            case RoutineType.DAYS_AFTER_3:
+                return basis_date + timedelta(days=3)
+            case _:
+                raise ValueError(f"RoutineType not found: {self}")
+
     @staticmethod
     def from_text(text: str) -> "RoutineType":
         for routine_type in RoutineType:
@@ -17,6 +43,7 @@ class RoutineType(Enum):
                 return routine_type
         msg = f"RoutineType not found: {text}"
         raise ValueError(msg)
+
 
 @dataclass(frozen=True)
 class RoutineTask:
@@ -28,3 +55,7 @@ class RoutineTask:
         return RoutineTask(
             title=title,
             routine_type=RoutineType.from_text(routine_type_text))
+
+    def needs_today(self) -> bool:
+        today = jst_today()
+        next_date = self.routine_type.next_date()
---
+++ test/domain/task/test_routine_type.py
@@ -0,0 +1,29 @@
+from datetime import date
+
+import pytest
+from notion_api.domain.task.routine_task import RoutineType
+
+# 2024年3月20日(水)
+BASIS_DATE_WED = date(2024, 3, 20)
+BASIS_DATE_SUN = date(2024, 3, 24)
+@pytest.mark.parametrize(
+    [
+        "basis_date",
+        "suite",
+        "expected"
+    ],
+    [
+        pytest.param(BASIS_DATE_WED, RoutineType.DAILY, date(2024, 3, 20), id="DAILY"),
+        pytest.param(BASIS_DATE_WED, RoutineType.EVERY_SAT, date(2024, 3, 23), id="EVERY_SAT"),
+        pytest.param(BASIS_DATE_WED, RoutineType.EVERY_TUE_AND_FRI, date(2024, 3, 22), id="EVERY_TUE_AND_FRI"),
+        pytest.param(BASIS_DATE_WED, RoutineType.EVERY_WED, date(2024, 3, 20), id="EVERY_WED"),
+        pytest.param(BASIS_DATE_WED, RoutineType.DAYS_AFTER_7, date(2024, 3, 27), id="DAYS_AFTER_7"),
+        pytest.param(BASIS_DATE_WED, RoutineType.DAYS_AFTER_3, date(2024, 3, 23), id="DAYS_AFTER_3"),
+        pytest.param(BASIS_DATE_SUN, RoutineType.DAILY, date(2024, 3, 24), id="DAILY_2"),
+        pytest.param(BASIS_DATE_SUN, RoutineType.EVERY_SAT, date(2024, 3, 30), id="EVERY_SAT_2"),
+        pytest.param(BASIS_DATE_SUN, RoutineType.EVERY_TUE_AND_FRI, date(2024, 3, 26), id="EVERY_TUE_AND_FRI_2"),
+        pytest.param(BASIS_DATE_SUN, RoutineType.EVERY_WED, date(2024, 3, 27), id="EVERY_WED_2"),
+    ]
+)
+def test_次のルーティン実行日を取得する(basis_date: date, suite: RoutineType, expected: date):
+    assert suite.next_date(basis_date) == expected
