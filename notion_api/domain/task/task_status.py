9fd2d40b417c960ac10b89fa848ff17874e9015c
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-20 22:01:54 +0900

    タスクステータスの変換メソッドを修正***

--- notion_api/domain/task/task_status.py
+++ notion_api/domain/task/task_status.py
@@ -14,23 +14,16 @@ class TaskStatusType(Enum):
     @staticmethod
     def from_text(text: str) -> "TaskStatusType":
         for status_type in TaskStatusType:
-            if status_type.value == text:
+            if status_type.value.lower() == text.lower():
                 return status_type
         msg = f"TaskStatusType に存在しない値です: {text}"
         raise ValueError(msg)
 
     @staticmethod
     def get_status_list(status_list: list[str]|None) -> list["TaskStatusType"]:
-        if status_list is not None or len(status_list) == 0:
-            return list(TaskStatusType)
-
-        result: list[TaskStatusType] = []
-        for status_str in status_list:
-            for status in TaskStatusType:
-                if status.value.upper() == status_str.upper():
-                    result.append(status)
-                    break
-        return result
+        if status_list is None:
+            return []
+        return [TaskStatusType.from_text(status) for status in status_list]
 
 class TaskStatus(Status):
     NAME = "ステータス"
---
+++ test/domain/task/test_task_status.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+from notion_api.domain.task.task_status import TaskStatusType
+
+
+class TestTaskStatusType(TestCase):
+    def test_変換する(self):
+        # Given
+        input = ["ToDO", "InProgress"]
+
+        # When
+        actual = TaskStatusType.get_status_list(input)
+
+        # Then
+        self.assertEqual(2, len(actual))
+        self.assertEqual("ToDo", actual[0].value)
+        self.assertEqual("InProgress", actual[1].value)
