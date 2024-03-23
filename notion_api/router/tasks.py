79f9eaf041cf233becfa0aa17200438a122dd648
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-23 13:31:11 +0900

    フォーマット

--- notion_api/router/tasks.py
+++ notion_api/router/tasks.py
@@ -12,17 +12,15 @@ router = APIRouter()
 @router.get("", response_model=TasksResponse)
 def get_tasks(start_date: date | None = None,
               status: str | None = None,
-              access_token: str | None = Header(None)):
+              access_token: str | None = Header(None)) -> TasksResponse:
     valid_access_token(access_token)
     status_list: list[str] = status.split(",") if status else []
     tasks = task.fetch_tasks(start_date=start_date,
                              status_list=status_list)
-    response = TasksResponse(data=[Task.from_params(t) for t in tasks])
-    return response
+    return TasksResponse(data=[Task.from_params(t) for t in tasks])
 
 @router.get("/current", response_model=TasksResponse)
-def get_current_tasks(access_token: str | None = Header(None)):
+def get_current_tasks(access_token: str | None = Header(None)) -> TasksResponse:
     valid_access_token(access_token)
     tasks = task.get_current_tasks()
-    response = TasksResponse(data=[Task.from_params(t) for t in tasks])
-    return response
+    return TasksResponse(data=[Task.from_params(t) for t in tasks])
