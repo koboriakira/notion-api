37d168469d6ac254dd1be75bda623d9682e906a5
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-23 13:01:43 +0900

    タスク関連のコードを削除しました

--- notion_api/interface/batch.py
+++ notion_api/interface/batch.py
@@ -1,10 +1,6 @@
-from datetime import date as DateObject
 
 from custom_logger import get_logger
-from infrastructure.current_task_s3_repository import CurrentTaskS3Repository
-from infrastructure.task.task_repository_impl import TaskRepositoryImpl
 from usecase.clean_empty_title_page import CleanEmptyTitlePageUsecase
-from usecase.fetch_tasks_usecase import FetchTasksUsecase
 from usecase.move_tasks_to_backup_usecase import MoveTasksToBackupUsecase
 
 logger = get_logger(__name__)
@@ -13,17 +9,6 @@ def clean_empty_title_page():
     usecase = CleanEmptyTitlePageUsecase()
     usecase.handle()
 
-def update_current_tasks() -> None:
-    """ いまやるタスクの一覧キャッシュを更新 """
-    usecase = FetchTasksUsecase(task_repository=TaskRepositoryImpl())
-    current_task_repository = CurrentTaskS3Repository()
-
-    tasks = usecase.execute(
-        status_list=["ToDo", "InProgress"],
-        start_date=DateObject.today())
-    current_task_repository.save(tasks)
-
-    return True
 
 def move_completed_task_to_backup() -> None:
     """ 実施日が過去のタスクをバックアップ """
