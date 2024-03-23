9cdc6b8f1806834b970409b649039d65f138b80f
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-23 13:27:42 +0900

    フォーマット

--- notion_api/router/tasks.py
+++ notion_api/router/tasks.py
@@ -1,17 +1,18 @@
+from datetime import date
+
 from fastapi import APIRouter, Header
-from typing import Optional
-from datetime import date as Date
+
 from interface import task
+from router.response import Task, TasksResponse
 from util.access_token import valid_access_token
-from router.response import TasksResponse, Task
 
 router = APIRouter()
 
 
 @router.get("", response_model=TasksResponse)
-def get_tasks(start_date: Optional[Date] = None,
-              status: Optional[str] = None,
-              access_token: Optional[str] = Header(None)):
+def get_tasks(start_date: date | None = None,
+              status: str | None = None,
+              access_token: str | None = Header(None)):
     valid_access_token(access_token)
     status_list: list[str] = status.split(",") if status else []
     tasks = task.fetch_tasks(start_date=start_date,
@@ -20,7 +21,7 @@ def get_tasks(start_date: Optional[Date] = None,
     return response
 
 @router.get("/current", response_model=TasksResponse)
-def get_current_tasks(access_token: Optional[str] = Header(None)):
+def get_current_tasks(access_token: str | None = Header(None)):
     valid_access_token(access_token)
     tasks = task.get_current_tasks()
     response = TasksResponse(data=[Task.from_params(t) for t in tasks])
