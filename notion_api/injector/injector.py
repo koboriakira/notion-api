ec1945ab7fdba9dff896002c13bbbb0585b92090
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-25 00:37:02 +0900

    コードの変更点を簡潔に説明するため、以下のコミットメッセージをご提案いたします。*** ***Injectorクラスにテキストサマライザーを追加

--- notion_api/injector/injector.py
+++ notion_api/injector/injector.py
@@ -1,3 +1,5 @@
+from logging import Logger
+
 from slack_sdk.web import WebClient
 
 from custom_logger import get_logger
@@ -5,26 +7,43 @@ from usecase.service.inbox_service import InboxService
 from usecase.service.openai_executer import OpenaiExecuter
 from usecase.service.tag_analyzer import TagAnalyzer
 from usecase.service.tag_create_service import TagCreateService
+from usecase.service.text_summarizer import TextSummarizer
 
 logger = get_logger(__name__)
 
 DEFAULT_GPT_MODEL = "gpt-3.5-turbo-1106"
 
 class Injector:
-    @staticmethod
-    def create_inbox_service() -> InboxService:
+    @classmethod
+    def create_inbox_service(cls: "Injector") -> InboxService:
         slack_client = WebClient()
         return InboxService(slack_client=slack_client)
 
-    @staticmethod
-    def create_tag_create_service() -> TagCreateService:
+    @classmethod
+    def create_tag_create_service(cls: "Injector") -> TagCreateService:
         return TagCreateService()
 
-    @staticmethod
-    def create_tag_analyzer(is_debug: bool|None=None) -> TagAnalyzer:
-        openai_executer = OpenaiExecuter(model=DEFAULT_GPT_MODEL, logger=logger)
+    @classmethod
+    def create_tag_analyzer(cls: "Injector", is_debug: bool|None=None) -> TagAnalyzer:
+        openai_executer = cls.__create_openai_executer(model=DEFAULT_GPT_MODEL, logger=logger)
         return TagAnalyzer(
             client=openai_executer,
             logger=logger,
             is_debug=is_debug,
         )
+
+    @classmethod
+    def create_text_summarizer(cls: "Injector", is_debug: bool|None=None) -> TextSummarizer:
+        openai_executer = cls.__create_openai_executer(model=DEFAULT_GPT_MODEL, logger=logger)
+        return TextSummarizer(
+            logger=logger,
+            client=openai_executer,
+            is_debug=is_debug,
+        )
+
+    @classmethod
+    def __create_openai_executer(
+        cls: "Injector",
+        model: str|None=None,
+        logger: Logger|None=None) -> TagAnalyzer:
+        return OpenaiExecuter(model=model, logger=logger)
