4abfb0dc475f4f8e3f34e9a8e87f7a7294f17b2e
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-27 02:24:44 +0900

    レストランリポジトリの実装を更新しました

--- notion_api/restaurant/infrastructure/restaurant_repository_impl.py
+++ notion_api/restaurant/infrastructure/restaurant_repository_impl.py
@@ -2,7 +2,9 @@ from logging import Logger, getLogger
 
 from notion_client_wrapper.client_wrapper import ClientWrapper
 from notion_client_wrapper.database.database_type import DatabaseType
+from notion_client_wrapper.filter.filter_builder import FilterBuilder
 from restaurant.domain.restaurant import Restaurant
+from restaurant.domain.restaurant_title import RestaurantName
 
 
 class RestaurantRepositoryImpl:
@@ -13,6 +15,20 @@ class RestaurantRepositoryImpl:
         self._client = client
         self._logger = logger or getLogger(__name__)
 
+    def find_by_title(self, title: str) -> Restaurant|None:
+        title_property = RestaurantName(text=title)
+        filter_param = FilterBuilder.build_simple_equal_condition(title_property)
+        searched_restaurant = self._client.retrieve_database(
+            database_id=DatabaseType.RESTAURANT.value,
+            filter_param=filter_param,
+            page_model=Restaurant,
+        )
+        if len(searched_restaurant) == 0:
+            return None
+        if len(searched_restaurant) > 1:
+            warning_message = f"Found multiple restaurant with the same title: {title}"
+            self._logger.warning(warning_message)
+        return searched_restaurant[0]
 
     def save(self, restaurant: Restaurant) -> Restaurant:
         result = self._client.create_page_in_database(
--- notion_api/restaurant/service/restaurant_creator.py
+++ notion_api/restaurant/service/restaurant_creator.py
@@ -1,16 +1,17 @@
 from logging import Logger, getLogger
 
-from webclip.domain.webclip import Webclip
+from restaurant.domain.restaurant import Restaurant
+from restaurant.infrastructure.restaurant_repository_impl import RestaurantRepositoryImpl as RestaurantRepository
 
 
 class RestaurantCreator:
     def __init__(
             self,
-            # webclip_repository: WebclipRepositoryImpl,
+            restaurant_repository: RestaurantRepository,
             # webclip_generator_rule: WebclipGeneratorRule,
             logger: Logger | None = None,
             ) -> None:
-        # self._webclip_repository = webclip_repository
+        self._restaurant_repository = restaurant_repository
         # self._webclip_generator_rule = webclip_generator_rule
         self._logger = logger or getLogger(__name__)
 
@@ -19,40 +20,41 @@ class RestaurantCreator:
             url: str,
             title: str | None = None,
             cover: str | None = None,
-            ) -> Webclip:
+            ) -> Restaurant:
+        if title is None:
+            msg = "title is required"
+            raise ValueError(msg)
+
         info_message = f"{self.__class__} execute: url={url}, title={title}, cover={cover}"
         self._logger.info(info_message)
 
-        webclip = self._find_webclip(title=title)
-        if webclip is not None:
-            return webclip
+        restaurant = self._restaurant_repository.find_by_title(title=title)
+        if restaurant is not None:
+            info_message = f"Restaurant is already registered: {restaurant.restaurant_name}"
+            self._logger.info(info_message)
+            return restaurant
 
-        # Webclipを生成
-        webclip_generator = self._webclip_generator_rule.get_generator(url=url)
-        webclip = webclip_generator.execute(
-            url=url,
+        info_message = "Create a Restaurant"
+        self._logger.info(info_message)
+
+        # Restaurantを生成
+        restaurant = Restaurant.create(
             title=title,
+            url=url,
             cover=cover,
         )
 
-        return self._webclip_repository.save(webclip=webclip)
+        return self._restaurant_repository.save(restaurant)
 
-    def _find_webclip(self, title: str) -> Webclip | None:
-        webclip = self._webclip_repository.find_by_title(title=title)
-        if webclip is not None:
-            info_message = f"Webclip is already registered: {webclip.get_title_text()}"
-            self._logger.info(info_message)
-            return webclip
-
-        info_message = "Create a Webclip"
-        self._logger.info(info_message)
-        return None
 
 if __name__ == "__main__":
-    # python -m notion_api.webclip.service.webclip_creator
-    from webclip.injector import WebclipInjector
-    service = WebclipInjector.create_webclip_creator()
-    service.execute(
-        url="https://hobby.dengeki.com/news/2222026/",
-        title="『爆走兄弟レッツ＆ゴー!!』のミニ四駆がミニカー化！マグナムセイバーなどの「トミカプレミアムunlimited」がAmazonで予約受付中!! | 電撃ホビーウェブ",
+    # python -m notion_api.restaurant.service.restaurant_creator
+    from notion_client_wrapper.client_wrapper import ClientWrapper
+    client = ClientWrapper.get_instance()
+    suite = RestaurantCreator(
+        restaurant_repository=RestaurantRepository(client=client),
+    )
+    suite.execute(
+        url="https://tabelog.com/tokyo/A1316/A131604/13020181/",
+        title="焼肉・光陽 (大崎/焼肉)",
     )
