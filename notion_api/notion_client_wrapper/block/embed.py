b237c89b756ea8ad04a4c4aab6e714383a7657aa
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-26 14:33:17 +0900

    フォーマット

--- notion_api/notion_client_wrapper/block/embed.py
+++ notion_api/notion_client_wrapper/block/embed.py
@@ -1,4 +1,3 @@
-
 from notion_client_wrapper.block.block import Block
 
 
@@ -6,8 +5,16 @@ class Embed(Block):
     caption: list
     embed_url: str
 
-    def __init__(self, id: str, archived: bool, created_time: str, last_edited_time: str, has_children: bool,
-                 parent: dict, caption: list, embed_url: str):
+    def __init__(
+            self,
+            id: str,
+            archived: bool,
+            created_time: str,
+            last_edited_time: str,
+            has_children: bool,
+            parent: dict,
+            caption: list,
+            embed_url: str):
         super().__init__(id, archived, created_time, last_edited_time, has_children, parent)
         self.caption = caption
         self.embed_url = embed_url
@@ -23,7 +30,7 @@ class Embed(Block):
             has_children=block["has_children"],
             parent=block["parent"],
             caption=embed["caption"],
-            embed_url=embed["url"] if "url" in embed else "",
+            embed_url=embed.get("url", ""),
         )
 
     @property
