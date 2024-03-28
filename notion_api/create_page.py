6e4d198a4cd79a7dc3aab6ba780d649cd6c55a00
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-28 14:40:52 +0900

    CreatePageUseCaseにCreatePageRequestを追加

--- notion_api/create_page.py
+++ notion_api/create_page.py
@@ -3,6 +3,7 @@ import logging
 
 from injector.injector import Injector
 from usecase.add_video_usecase import AddVideoUsecase
+from usecase.create_page_use_case import CreatePageRequest
 from util.environment import Environment
 from util.error_reporter import ErrorReporter
 
@@ -11,49 +12,35 @@ logging.basicConfig(level=logging.INFO)
 if Environment.is_dev():
     logging.basicConfig(level=logging.DEBUG)
 
-def execute(  # noqa: PLR0913
-        url: str,
-        title: str | None = None,
-        cover: str | None = None,
-        slack_channel: str | None = None,
-        slack_thread_ts: str | None = None,
-        params: dict|None = None, # いつか使うかも  # noqa: ARG001
-        ) -> dict:
+def execute(request_param: CreatePageRequest) -> dict:
     usecase = Injector.create_page_use_case()
-    return usecase.execute(url=url, title=title, cover=cover, slack_channel=slack_channel, slack_thread_ts=slack_thread_ts)
+    return usecase.execute(request_param=request_param)
 
 
 def handler(event:dict, context:dict) -> dict:  # noqa: ARG001
     request: dict = json.loads(event["Records"][0]["body"])
     print("request", request)
     mode: str = request["mode"]
-    params: dict = request["params"]
-    slack_channel=params.get("slack_channel")
-    slack_thread_ts=params.get("slack_thread_ts")
+    request_param = CreatePageRequest.from_params(request["params"])
     try:
         if mode == "video":
             usecase = AddVideoUsecase()
             _ = usecase.execute(
-                url=params["url"],
-                title=params["title"],
-                cover=params.get("cover"),
-                slack_channel=slack_channel,
-                slack_thread_ts=slack_thread_ts,
+                url=request_param.url,
+                title=request_param.title,
+                cover=request_param.cover,
+                slack_channel=request_param.slack_channel,
+                slack_thread_ts=request_param.slack_thread_ts,
                 )
             return {}
 
         # いずれは上記2つもここに統合する
-        execute(
-            url=params["url"],
-            title=params["title"],
-            cover=params.get("cover"),
-            slack_channel=slack_channel,
-            slack_thread_ts=slack_thread_ts)
+        execute(request_param)
         return {}
     except Exception:  # noqa: BLE001
         ErrorReporter().execute(
-            slack_channel=slack_channel,
-            slack_thread_ts=slack_thread_ts,
+            slack_channel=request_param.slack_channel,
+            slack_thread_ts=request_param.slack_thread_ts,
     )
 
 if __name__ == "__main__":
--- notion_api/usecase/create_page_use_case.py
+++ notion_api/usecase/create_page_use_case.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from logging import Logger
 from typing import TYPE_CHECKING
 
@@ -9,6 +10,23 @@ from usecase.service.inbox_service import InboxService
 if TYPE_CHECKING:
     from common.service.page_creator import PageCreator
 
+@dataclass(frozen=True)
+class CreatePageRequest:
+    url: str
+    title: str | None = None
+    cover: str | None = None
+    slack_channel: str | None = None
+    slack_thread_ts: str | None = None
+
+    @staticmethod
+    def from_params(params: dict) -> "CreatePageRequest":
+        return CreatePageRequest(
+            url=params["url"],
+            title=params.get("title"),
+            cover=params.get("cover"),
+            slack_channel=params.get("slack_channel"),
+            slack_thread_ts=params.get("slack_thread_ts"),
+        )
 
 class CreatePageUseCase:
     def __init__(
@@ -23,33 +41,28 @@ class CreatePageUseCase:
         self._append_context_service = append_context_service
         self._logger = logger
 
-    def execute(  # noqa: PLR0913
+    def execute(
             self,
-            url: str,
-            title: str | None = None,
-            cover: str | None = None,
-            slack_channel: str | None = None,
-            slack_thread_ts: str | None = None,
-            ) -> dict:
+            request: CreatePageRequest) -> dict:
         # どのデータベースのページを作るべきか判断して、作成する
-        site_kind = SiteKind.find_site_kind(url=url)
+        site_kind = SiteKind.find_site_kind(url=request.url)
         page_creator:PageCreator = self._page_creator_factory.get_creator(site_kind=site_kind)
         self._logger.debug(f"site_kind={site_kind.value}, page_creator={type(page_creator)}")
-        page = page_creator.execute(url=url, title=title, cover=cover)
+        page = page_creator.execute(url=request.url, title=request.title, cover=request.cover)
 
         # Inboxにタスクを追加
         self._inbox_service.add_inbox_task_by_page_id(
             page_id=page.id,
             page_url=page.url,
-            original_url=url,
-            slack_channel=slack_channel,
-            slack_thread_ts=slack_thread_ts,
+            original_url=request.url,
+            slack_channel=request.slack_channel,
+            slack_thread_ts=request.slack_thread_ts,
         )
 
         # コンテキストにページIDを追加
         self._append_context_service.append_page_id(
-            channel=slack_channel,
-            event_ts=slack_thread_ts,
+            channel=request.slack_channel,
+            event_ts=request.slack_thread_ts,
             page_id=page.id,
         )
 
