85e73436fcd3b4772ab376ad0f185783b5559509
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-28 15:32:21 +0900

    タグの説明文を更新しました

--- notion_api/util/tag_analyzer.py
+++ notion_api/util/tag_analyzer.py
@@ -24,7 +24,7 @@ class TagAnalyzer:
     def handle(self, text: str) -> list[str]:
         if self.is_debug:
             return ["テスト"]
-        user_content = f"次の文章を解析して、タグをつけてください。タグはたとえば人名やカテゴリ、特定のキーワードのようなものを指します。\n\n{text}"
+        user_content = f"次の文章を解析して、タグをつけてください。タグはたとえば人名や作品名、カテゴリ、特定の重要なキーワードのようなものを指します。\n\n{text}"
         analyze_tags_parameters = {
             "type": "object",
             "properties": {
