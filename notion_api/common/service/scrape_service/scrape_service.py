c765ef1227846a2c95c91a7a593a6a865e114b11
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-28 15:50:08 +0900

    ScrapeServiceのスクレイパーをDefaultScraperからScraperに変更

--- notion_api/common/service/scrape_service/scrape_service.py
+++ notion_api/common/service/scrape_service/scrape_service.py
@@ -1,9 +1,9 @@
-from common.infrastructure.default_scraper import DefaultScraper
+from common.infrastructure.default_scraper import DefaultScraper as Scraper
 from common.service.scrape_service.response import Response
 
 
 class ScrapeService:
-    def __init__(self, scraper: DefaultScraper) -> None:
+    def __init__(self, scraper: Scraper) -> None:
         self._scraper = scraper
 
     def execute(self, url: str) -> Response:
@@ -14,3 +14,9 @@ class ScrapeService:
             ogp_tags=scraped_result.ogp_tags.values,
             other_meta_tags=scraped_result.other_meta_tags.values,
         )
+
+if __name__ == "__main__":
+    # python -m notion_api.common.service.scrape_service.scrape_service
+    service = ScrapeService(scraper=Scraper())
+    result = service.execute("https://www.youtube.com/watch?v=82KT4FNyNdY")
+    print(result.get_image_url())
