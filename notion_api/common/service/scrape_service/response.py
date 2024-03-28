7d0c44a3cf749d3fd8864841e4cf9cee740b5a90
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-28 15:44:46 +0900

    Add get_image_url method to Response class

--- notion_api/common/service/scrape_service/response.py
+++ notion_api/common/service/scrape_service/response.py
@@ -7,3 +7,7 @@ class Response:
     formatted_text: str
     ogp_tags: dict
     other_meta_tags: dict
+
+    def get_image_url(self) -> str|None:
+        # 他にあればもっと付け足す
+        return self.ogp_tags.get("image") or self.other_meta_tags.get("image")
