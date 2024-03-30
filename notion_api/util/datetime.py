4ab6bb0b20bd308eb101efb69e847dd57f2eb172
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-30 14:04:43 +0900

    フォーマット

--- notion_api/util/datetime.py
+++ notion_api/util/datetime.py
@@ -2,19 +2,23 @@ from datetime import date, datetime, timedelta, timezone
 
 JST = timezone(timedelta(hours=+9), "JST")
 
+
 def jst_now() -> datetime:
     return datetime.now(JST)
+
 
 def jst_today_datetime() -> datetime:
     return jst_now().replace(hour=0, minute=0, second=0, microsecond=0)
 
+
 def jst_today() -> date:
     return jst_now().date()
 
-def convert_to_date_or_datetime(value: str|None, cls: type|None = None) -> date | datetime | None:
+
+def convert_to_date_or_datetime(value: str | None, cls: type | None = None) -> date | datetime | None:
     if value is None:
         return None
-    length_date = 10 # "YYYY-MM-DD"
+    length_date = 10  # "YYYY-MM-DD"
     value_error_msg = f"Invalid class: {cls}"
     if len(value) == length_date:
         tmp_date = date.fromisoformat(value)
