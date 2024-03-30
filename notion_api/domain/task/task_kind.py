36ff42ad0522b64c8269cf7b297846b47b90fdeb
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-30 14:26:12 +0900

    テストを追加

--- notion_api/domain/task/task_kind.py
+++ notion_api/domain/task/task_kind.py
@@ -3,31 +3,32 @@ from enum import Enum
 from notion_client_wrapper.properties.select import Select
 
 kind_map = {
-  "次にとるべき行動リスト": {
-    "selected_id": "d73dbc77-702d-4b2e-88e1-22b54a17a333",
-    "selected_color": "brown",
-  },
-  "今すぐやる": {
-    "selected_id": "a0cab03e-aa5c-4619-835b-7f1cedd6500f",
-    "selected_color": "pink",
-  },
-  "いつかやる・たぶんやる": {
-    "selected_id": "66d35bb2-7f64-41d3-8ec8-48f025e47236",
-    "selected_color": "orange",
-  },
-  "ゴミ箱": {
-    "selected_id": "e4179710-03ef-43c0-8fb3-01b463e25edd",
-    "selected_color": "purple",
-  },
-  "待ち": {
-    "selected_id": "8c1685c7-5398-4cea-b950-b874501a7713",
-    "selected_color": "gray",
-  },
-  "スケジュール": {
-    "selected_id": "a6e6329d-f547-44d4-b418-ac239dd88632",
-    "selected_color": "blue",
-  },
+    "次にとるべき行動リスト": {
+        "selected_id": "d73dbc77-702d-4b2e-88e1-22b54a17a333",
+        "selected_color": "brown",
+    },
+    "今すぐやる": {
+        "selected_id": "a0cab03e-aa5c-4619-835b-7f1cedd6500f",
+        "selected_color": "pink",
+    },
+    "いつかやる・たぶんやる": {
+        "selected_id": "66d35bb2-7f64-41d3-8ec8-48f025e47236",
+        "selected_color": "orange",
+    },
+    "ゴミ箱": {
+        "selected_id": "e4179710-03ef-43c0-8fb3-01b463e25edd",
+        "selected_color": "purple",
+    },
+    "待ち": {
+        "selected_id": "8c1685c7-5398-4cea-b950-b874501a7713",
+        "selected_color": "gray",
+    },
+    "スケジュール": {
+        "selected_id": "a6e6329d-f547-44d4-b418-ac239dd88632",
+        "selected_color": "blue",
+    },
 }
+
 
 class TaskKindType(Enum):
     TRASH = "ゴミ箱"
---
+++ test/task/infrastructure/test_task_repository_impl.py
@@ -0,0 +1,66 @@
+from datetime import date
+from unittest import TestCase
+from unittest.mock import Mock
+
+from notion_api.domain.database_type import DatabaseType
+from notion_api.domain.task.task import Task
+from notion_api.domain.task.task_kind import TaskKindType
+from notion_api.domain.task.task_status import TaskStatusType
+from notion_api.infrastructure.task.task_repository_impl import TaskRepositoryImpl
+from notion_api.notion_client_wrapper.client_wrapper import ClientWrapper
+
+
+class TestTaskRepositoryImpl(TestCase):
+    def setUp(self) -> None:
+        mock_client = Mock(spec=ClientWrapper)
+        self.suite = TaskRepositoryImpl(notion_client_wrapper=mock_client)
+        return super().setUp()
+
+    def test_search(self):
+        # Given
+        status_list = [TaskStatusType.TODO, TaskStatusType.IN_PROGRESS]
+        task_kind_type_list = [TaskKindType.NEXT_ACTION]
+        start_date = date(2024, 1, 2)
+
+        # When
+        _ = self.suite.search(status_list=status_list, kind_type_list=task_kind_type_list, start_date=start_date)
+
+        # Then
+        # クラス名の一致をチェックするロジックがあるので、完全に合わせておく
+        import sys
+
+        sys.path.append("notion_api")
+        from domain.task.task import Task as TaskModel
+
+        self.suite.client.retrieve_database.assert_called_once_with(
+            database_id=DatabaseType.TASK.value,
+            filter_param={
+                "and": [
+                    {"property": "タスク種別", "select": {"does_not_equal": "ゴミ箱"}},
+                    {"property": "実施日", "date": {"on_or_after": "2024-01-02T00:00:00+09:00"}},
+                    {"property": "実施日", "date": {"before": "2024-01-02T23:59:59.999999+09:00"}},
+                    {"or": [{"property": "タスク種別", "select": {"equals": "次にとるべき行動リスト"}}]},
+                    {
+                        "or": [
+                            {"property": "ステータス", "status": {"equals": "ToDo"}},
+                            {"property": "ステータス", "status": {"equals": "InProgress"}},
+                        ]
+                    },
+                ]
+            },
+            page_model=TaskModel,
+        )
+
+    def test_タスクを保存する(self):
+        # Given
+        task = Task.create(
+            title="title",
+        )
+        self.suite.client.create_page_in_database.return_value = {"id": "dummy-id"}
+
+        # When
+        _ = self.suite.save(task=task)
+
+        # Then
+        self.suite.client.create_page_in_database.assert_called_once()
+        self.suite.client.retrieve_page.assert_called_once()
