a3cb0993c184a30daa74c5f14df9cfd9c4e4a491
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-04-06 20:23:47 +0900

    不要なソースを削除

--- notion_api/interface/project.py
+++ notion_api/interface/project.py
@@ -1,28 +1,14 @@
-from typing import Optional
-from datetime import date as DateObject
-from usecase.project_list_usecase import ProjectListUseCase
-from usecase.project_find_usecase import ProjectFindUsecase
-from domain.project.project_status import ProjectStatus
+from datetime import date
+
 from custom_logger import get_logger
 
 logger = get_logger(__name__)
 
-def get_projects(status: Optional[str] = None, remind_date: Optional[DateObject] = None, is_thisweek: bool = False):
-    """ プロジェクト一覧を取得 """
-    status_list = ProjectStatus.get_status_list(status)
-    logger.debug(status_list)
-    detail_enabled = True if status is not None else False
-    logger.debug(detail_enabled)
-    usecase = ProjectListUseCase()
-    result = usecase.execute(status_list=status_list,
-                    remind_date=remind_date,
-                    detail_enabled=detail_enabled,
-                    thisweek_filter_enabled=is_thisweek)
-    return result
+
+def get_projects(status: str | None = None, remind_date: date | None = None, is_thisweek: bool = False):
+    raise NotImplementedError
 
 
 def find_project(project_id: str):
-    """ プロジェクト一覧を取得 """
-    usecase = ProjectFindUsecase()
-    result = usecase.execute(project_id=project_id)
-    return result
+    """プロジェクト一覧を取得"""
+    raise NotImplementedError
--- notion_api/router/response/projects_response.py
+++ notion_api/router/response/projects_response.py
@@ -1,24 +1,23 @@
 from pydantic import Field
-import json
-from typing import Optional
-from router.response.base_response import BaseResponse
-from router.response.base_notion_page_model import BaseNotionPageModel
-from domain.project.project_status import ProjectStatus
+
 from custom_logger import get_logger
+from router.response.base_notion_page_model import BaseNotionPageModel
+from router.response.base_response import BaseResponse
 
 logger = get_logger(__name__)
 
+
 class Project(BaseNotionPageModel):
     goal_id_list: list[str] = Field(default=[])
-    status: Optional[ProjectStatus]
-    completed_at: Optional[str]
-    recursive_conf: Optional[str]
-    remind_date: Optional[str]
+    status: str | None
+    completed_at: str | None
+    recursive_conf: str | None
+    remind_date: str | None
     is_thisweek: bool
 
     @staticmethod
     def from_params(params: dict) -> "Project":
-        logger.debug(f"params:")
+        logger.debug("params:")
         logger.debug(params)
         return Project(
             id=params["id"],
@@ -27,15 +26,17 @@ class Project(BaseNotionPageModel):
             created_at=params["created_at"],
             updated_at=params["updated_at"],
             goal_id_list=params["goal_id_list"],
-            status=ProjectStatus(params["status"]),
+            status=params["status"],
             completed_at=params["completed_at"],
             recursive_conf=params["recursive_conf"],
             remind_date=params["remind_date"],
             is_thisweek=params["is_thisweek"],
         )
 
+
 class ProjectResponse(BaseResponse):
-    data: Optional[Project]
+    data: Project | None
+
 
 class ProjectsResponse(BaseResponse):
     data: list[Project] = Field(default=[])
