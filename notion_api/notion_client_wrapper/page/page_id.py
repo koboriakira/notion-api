0d7f0b3ba7e8522c1b31850c10fc002041e1f7c5
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-04-06 22:00:12 +0900

    page_idの形式チェックを修正

--- notion_api/notion_client_wrapper/page/page_id.py
+++ notion_api/notion_client_wrapper/page/page_id.py
@@ -10,6 +10,6 @@ class PageId:
         # 5c38fd30714b4ce2bf2d25407f3cfc16
         # 5c38fd30-714b-4ce2-bf2d-25407f3cfc16
         # どちらかの形式であることを確認する
-        if not re.match(r"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}", self.value):
+        if not re.match(r"[0-9a-f]{8}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{12}", self.value):
             msg = f"page_idの形式が不正です: {self.value}"
             raise ValueError(msg)
