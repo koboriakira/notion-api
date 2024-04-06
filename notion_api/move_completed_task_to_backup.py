c2762d2100da2edb6751fa4231b11f6c6af4d867
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-04-07 08:42:41 +0900

    notion_api/move_completed_task_to_backup.pyの変更を追加

--- notion_api/move_completed_task_to_backup.py
+++ notion_api/move_completed_task_to_backup.py
@@ -1,5 +1,7 @@
 import logging
 
+from notion_api.notion_client_wrapper.client_wrapper import ClientWrapper
+from notion_api.task.infrastructure.task_repository_impl import TaskRepositoryImpl
 from usecase.move_tasks_to_backup_usecase import MoveTasksToBackupUsecase
 from util.environment import Environment
 from util.error_reporter import ErrorReporter
@@ -12,7 +14,12 @@ if Environment.is_dev():
 
 def handler(event: dict, context: dict) -> None:  # noqa: ARG001
     try:
-        usecase = MoveTasksToBackupUsecase()
+        task_repository = TaskRepositoryImpl(
+            notion_client_wrapper=ClientWrapper.get_instance(),
+        )
+        usecase = MoveTasksToBackupUsecase(
+            task_repository=task_repository,
+        )
         usecase.execute()
     except:
         ErrorReporter().execute()
