4bd193be36f5fa7f11e432751e3cbe38e1fa3d0b
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-04-11 23:59:24 +0900

    タグ関連のコードを更新しました

--- notion_api/common/domain/tag_relation.py
+++ notion_api/common/domain/tag_relation.py
@@ -1,13 +1,18 @@
 from common.value.notion_page_id import NotionPageId
+from notion_client_wrapper.page.page_id import PageId
 from notion_client_wrapper.properties.relation import Relation
+
+
+def convert_page_id(id_: str | NotionPageId | PageId) -> str:
+    if isinstance(id_, PageId):
+        return id_.value
+    return id_.value if isinstance(id_, NotionPageId) else id_
 
 
 class TagRelation(Relation):
     NAME = "タグ"
 
-    def __init__(
-            self,
-            id_list: list[str]|None = None) -> None:
+    def __init__(self, id_list: list[str] | None = None) -> None:
         super().__init__(
             name=self.NAME,
             id_list=list(set(id_list or [])),
@@ -18,11 +23,9 @@ class TagRelation(Relation):
         return TagRelation(id_list=[])
 
     @staticmethod
-    def from_id_list(id_list: list[str|NotionPageId]) -> "TagRelation":
-        return TagRelation(
-            id_list=[id_.value if isinstance(id_, NotionPageId) else id_ for id_ in id_list],
-        )
+    def from_id_list(id_list: list[str | NotionPageId]) -> "TagRelation":
+        return TagRelation(id_list=[convert_page_id(id_) for id_ in id_list])
 
-    def add(self, notion_page_id: NotionPageId|str) -> "TagRelation":
+    def add(self, notion_page_id: NotionPageId | str) -> "TagRelation":
         str_value = notion_page_id.value if isinstance(notion_page_id, NotionPageId) else notion_page_id
         return TagRelation(id_list=[*self.id_list, str_value])
