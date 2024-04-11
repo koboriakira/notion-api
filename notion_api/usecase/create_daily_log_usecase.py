19db4a2d68a78bebc0bcbcd24f7ed2a0c4236c63
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-04-12 01:38:59 +0900

    デイリーログ作成ユースケースの変更を追加

--- notion_api/usecase/create_daily_log_usecase.py
+++ notion_api/usecase/create_daily_log_usecase.py
@@ -1,15 +1,13 @@
-import os
-from datetime import date, datetime, timedelta
-
-import requests
+import contextlib
+from datetime import date
 
 from common.value.database_type import DatabaseType
-from daily_log.domain.daily_log_builder import DailyLogBuilder
 from daily_log.domain.daily_log_repository import DailyLogRepository
+from daily_log.infrastructure.daily_log_repository_impl import ExistedDailyLogError
+from daily_log.value.isoweek import Isoweek
 from notion_client_wrapper.client_wrapper import ClientWrapper
 from notion_client_wrapper.page.page_id import PageId
 from notion_client_wrapper.properties import Date, Title
-from util.datetime import JST, jst_today
 
 
 class CreateDailyLogUsecase:
@@ -17,21 +15,18 @@ class CreateDailyLogUsecase:
         self.client = client
         self._daily_log_repository = daily_log_repository
 
-    def handle(self, year: int | None = None, isoweeknum: int | None = None) -> None:
-        # 初期化
-        year = year or jst_today().year
-        isoweeknum = isoweeknum or jst_today().isocalendar()[1]
+    def handle(self, date_: date) -> None:
+        # ISO週情報を生成
+        isoweek = Isoweek.of(date_)
 
         # ウィークリーログを作成
-        weekly_log_entity = self._create_weekly_log_page(year, isoweeknum)
-        weekly_log_id = weekly_log_entity["id"]
+        weekly_log_entity = self._create_weekly_log_page(isoweek)
+        weekly_log_id = PageId(weekly_log_entity["id"])
 
-        # 開始日から終了日までのデイリーログを作成
-        # 指定さらた年とISO週から開始日、終了日を取得
-        start_date = generate_date(year, isoweeknum)
-        for i in range(7):
-            daily_date = start_date + timedelta(days=i)
-            self._create_daily_log_page(date_=daily_date, weekly_log_id=weekly_log_id)
+        # デイリーログを作成
+        for date_ in isoweek.date_range():
+            with contextlib.suppress(ExistedDailyLogError):
+                self._daily_log_repository.create(date_=date_, weekly_log_id=weekly_log_id)
 
     def _find_weekly_log(self, year: int, isoweeknum: int) -> dict | None:
         title = f"{year}-Week{isoweeknum}"
@@ -53,55 +48,15 @@ class CreateDailyLogUsecase:
             "goal": goal.text,
         }
 
-    def _create_weekly_log_page(self, year: int, isoweeknum: int) -> dict:
-        weekly_log_entity = self._find_weekly_log(year, isoweeknum)
+    def _create_weekly_log_page(self, isoweek: Isoweek) -> dict:
+        weekly_log_entity = self._find_weekly_log(isoweek.year, isoweek.isoweeknum)
         if weekly_log_entity is not None:
             return weekly_log_entity
-
-        title_text = f"{year}-Week{isoweeknum}"
-        start_date = generate_date(year, isoweeknum)
-        end_date = start_date + timedelta(days=6)
 
         return self.client.create_page_in_database(
             database_id=DatabaseType.WEEKLY_LOG.value,
             properties=[
-                Title.from_plain_text(name="名前", text=title_text),
-                Date.from_range(name="期間", start=start_date, end=end_date),
+                Title.from_plain_text(name="名前", text=str(isoweek)),
+                Date.from_range(name="期間", start=isoweek.start_date, end=isoweek.end_date),
             ],
         )
-
-    def _create_daily_log_page(self, date_: date, weekly_log_id: str) -> None:
-        daily_log = self._daily_log_repository.find(date_)
-        if daily_log is not None:
-            return
-
-        yesterday_daily_log = self._daily_log_repository.find(date_ - timedelta(days=1))
-        if yesterday_daily_log is None:
-            msg = "前日のデイリーログが存在しません"
-            raise ValueError(msg)
-
-        cover_url = get_random_photo_url()
-
-        daily_log = (
-            DailyLogBuilder.of(date_=date_)
-            .add_weekly_log_relation(weekly_log_page_id=PageId(weekly_log_id))
-            .add_previous_relation(previous_page_id=PageId(yesterday_daily_log.id))
-            .add_cover(cover_url=cover_url)
-            .build()
-        )
-        self._daily_log_repository.save(daily_log)
-
-
-def generate_date(year: int, isoweeknum: int) -> date:
-    return datetime.strptime(f"{year}-{isoweeknum}-1", "%G-%V-%u").replace(tzinfo=JST).date()
-
-
-def get_random_photo_url() -> str | None:
-    query = "bird,flower"
-    unsplash_access_key = os.getenv("UNSPLASH_ACCESS_KEY")
-    unsplash_api_url = f"https://api.unsplash.com/photos/random/?client_id={unsplash_access_key}&query={query}"
-    response = requests.get(unsplash_api_url, timeout=15)
-    if response.status_code != 200:
-        return None
-    data = response.json()
-    return data["urls"]["full"]
