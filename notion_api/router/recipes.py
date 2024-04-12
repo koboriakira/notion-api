39eb9184e501fefff197159b7dce8ae675d6758d
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-04-12 17:23:17 +0900

    レシピの詳細表示を有効にするための変更を追加

--- notion_api/router/recipes.py
+++ notion_api/router/recipes.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter, Header
-from typing import Optional
+
 from interface import recipe
+from router.response.recipe_response import Recipe, RecipesResponse
 from util.access_token import valid_access_token
-from router.response.recipe_response import RecipesResponse, Recipe
 
 router = APIRouter()
 
 
 @router.get("", response_model=RecipesResponse)
-def get_recipes(detail_enabled: bool = False,
-                access_token: Optional[str] = Header(None),
-                ):
+def get_recipes(
+    detail_enabled: bool | None = None,
+    access_token: str | None = Header(None),
+) -> RecipesResponse:
     valid_access_token(access_token)
-    recipes = recipe.get_recipes(detail_enabled)
+    recipes = recipe.get_recipes(detail_enabled or False)
     return RecipesResponse(data=[Recipe.from_params(p) for p in recipes])
