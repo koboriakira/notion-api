8b1ba076897f01c7246e74f0a0cb6a8d6c71942c
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-04-12 14:51:51 +0900

    TypeSafeFrozenListクラスを追加し、TypeSafeFrozenObjectのリストを保持するようにしました。リスト内の要素がTypeSafeFrozenObjectであることを確認するために、__post_init__メソッドを追加しました。

--- notion_api/util/dataclass/type_safe_object.py
+++ notion_api/util/dataclass/type_safe_object.py
@@ -19,3 +19,14 @@ class TypeSafeFrozenObject:
             if not isinstance(value, self.__annotations__[field]):
                 msg = f"[{type(self)} Invalid type for {field}: {type(value)}"
                 raise TypeError(msg)
+
+
+@dataclass(frozen=True)
+class TypeSafeFrozenList:
+    values: list[TypeSafeFrozenObject]
+
+    def __post_init__(self) -> None:
+        for value in self.values:
+            if not isinstance(value, TypeSafeFrozenObject):
+                msg = f"[{type(self)} Invalid type for {value}: {type(value)}"
+                raise TypeError(msg)
