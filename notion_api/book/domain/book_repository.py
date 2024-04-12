0a32e07463ad5152eca15041700795b8653d62a1
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-04-12 10:20:14 +0900

    書籍リポジトリの変更を追加

--- notion_api/book/domain/book_repository.py
+++ notion_api/book/domain/book_repository.py
@@ -3,6 +3,22 @@ from abc import ABCMeta, abstractmethod
 from book.domain.book import Book
 
 
+class ExistedBookError(Exception):
+    """既に書籍ページが存在するエラー"""
+
+    def __init__(self, title: str) -> None:
+        self.title = title
+        super().__init__(f"Existed book page for {title}")
+
+
+class NotFoundBookError(Exception):
+    """書籍ページが存在しないエラー"""
+
+    def __init__(self, title: str) -> None:
+        self.title = title
+        super().__init__(f"Not found book page for {title}")
+
+
 class BookRepository(metaclass=ABCMeta):
     @abstractmethod
     def find_by_title(self, title: str) -> Book | None:
--- notion_api/book/infrastructure/book_repository_impl.py
+++ notion_api/book/infrastructure/book_repository_impl.py
@@ -1,11 +1,9 @@
 from logging import Logger, getLogger
 
 from book.domain.book import Book
-from book.domain.book_repository import BookRepository
-from book.domain.book_title import BookTitle
+from book.domain.book_repository import BookRepository, ExistedBookError, NotFoundBookError
 from notion_client_wrapper.client_wrapper import ClientWrapper
 from notion_client_wrapper.database.database_type import DatabaseType
-from notion_client_wrapper.filter.filter_builder import FilterBuilder
 
 DATABASE_ID = DatabaseType.BOOK.value
 
@@ -16,21 +14,14 @@ class BookRepositoryImpl(BookRepository):
         self._logger = logger or getLogger(__name__)
 
     def find_by_title(self, title: str) -> Book | None:
-        title_property = BookTitle(text=title)
-        filter_param = FilterBuilder.build_simple_equal_condition(title_property)
-        searched_book = self._client.retrieve_database(
-            database_id=DATABASE_ID,
-            filter_param=filter_param,
-            page_model=Book,
-        )
-        if len(searched_book) == 0:
-            return None
-        if len(searched_book) > 1:
-            warning_message = f"Found multiple book with the same title: {title}"
-            self._logger.warning(warning_message)
-        return searched_book[0]
+        book = self.__find_by_title(title)
+        if book is None:
+            raise NotFoundBookError(title)
+        return book
 
     def save(self, book: Book) -> Book:
+        if self.__find_by_title(book.title) is None:
+            raise ExistedBookError(book.title)
         result = self._client.create_page_in_database(
             database_id=DATABASE_ID,
             properties=book.properties.values,
@@ -42,3 +33,11 @@ class BookRepositoryImpl(BookRepository):
             url=result["url"],
         )
         return book
+
+    def __find_by_title(self, title: str) -> Book | None:
+        return self._client.find_page_by_title(
+            database_id=DATABASE_ID,
+            title=title,
+            title_key_name="Title",
+            page_model=Book,
+        )
