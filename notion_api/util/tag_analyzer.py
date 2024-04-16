663ba4e2f0323b1b1e0dafdf93e0fac5e3560164
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-04-16 19:23:15 +0900

    タグ解析関数の修正とテストの追加

--- notion_api/util/tag_analyzer.py
+++ notion_api/util/tag_analyzer.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 from util.openai_executer import OpenaiExecuter
@@ -11,18 +12,24 @@ def prittier(tags: list[str]) -> list[str]:
     # Noneと空文字を削除
     return [tag for tag in tags if tag is not None and tag != ""]
 
-def analyze_tags(args: dict) -> list[str]:
-    if "tags" not in args or args["tags"] is None:
+
+def analyze_tags(args: str) -> list[str]:
+    print(args)
+    print(type(args))
+    json_dict = json.loads(args)
+    if "tags" not in json_dict or json_dict["tags"] is None:
         return []
-    arg_tags:str = args["tags"]
-    return prittier(arg_tags.split(","))
+    tags_str: str = json_dict["tags"]
+    return prittier(tags_str.split(","))
+
 
 class TagAnalyzer:
     def __init__(
-            self,
-            client: OpenaiExecuter | None = None,
-            logger: logging.Logger | None = None,
-            is_debug: bool|None = None) -> None:
+        self,
+        client: OpenaiExecuter | None = None,
+        logger: logging.Logger | None = None,
+        is_debug: bool | None = None,
+    ) -> None:
         self.client = client or OpenaiExecuter(model="gpt-3.5-turbo-1106", logger=logger)
         self.logger = logger or logging.getLogger(__name__)
         self.is_debug = is_debug or False
@@ -41,16 +48,10 @@ class TagAnalyzer:
             },
             "required": ["tags"],
         }
-        tags:list[str] = self.client.simple_function_calling(
+        tags: list[str] = self.client.simple_function_calling(
             user_content=user_content,
             func=analyze_tags,
             func_description="タグをつける",
             parameters=analyze_tags_parameters,
         )
         return tags
-
-if __name__ == "__main__":
-    # python -m notion_api.util.tag_analyzer
-    logging.basicConfig(level=logging.DEBUG)
-    tag_analyzer = TagAnalyzer()
-    print(tag_analyzer.handle("団体を引っ張る準備、できてます。練習量に裏付けされた確固たる自信!! デビュー前から続く現王者との歴史を振り返る。3.31両国国技館で王者・山下実優に挑む渡辺未詩にインタビュー"))
--- test/util/test_tag_analyzer.py
+++ test/util/test_tag_analyzer.py
@@ -1,13 +1,26 @@
-from notion_api.util.tag_analyzer import analyze_tags
+import json
+from unittest import TestCase
+
+import pytest
+from notion_api.util.tag_analyzer import TagAnalyzer, analyze_tags
 
 
 def test_analyze_tags():
-    input = {
-        "tags": "test1, test2, test3, test1"
-    }
+    input = {"tags": "test1, test2, test3, test1"}
 
-    actual = analyze_tags(args=input)
+    actual = analyze_tags(args=json.dumps(input))
     assert len(actual) == 3
     assert "test1" in actual
     assert "test2" in actual
     assert "test3" in actual
+
+
+class TestTagAnalyzer(TestCase):
+    @pytest.mark.skip(reason="実際にOpenAIを実行してコストがかかるためスキップ。")
+    def test_analyze_tags_actually(self):
+        suite = TagAnalyzer()
+
+        actual = suite.handle(
+            "団体を引っ張る準備、できてます。練習量に裏付けされた確固たる自信!! デビュー前から続く現王者との歴史を振り返る。3.31両国国技館で王者・山下実優に挑む渡辺未詩にインタビュー"
+        )
+        self.assertTrue(len(actual) > 0)
