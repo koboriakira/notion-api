256e44a2227cfb7620309f3678fc81ac7db3177d
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-05-27 14:36:58 +0900

    タスクAPIの修正*** ***タスクAPIの修正を行いました。

--- notion_api/router/tasks.py
+++ notion_api/router/tasks.py
@@ -10,14 +10,16 @@ router = APIRouter()
 
 
 @router.get("", response_model=TasksResponse)
-def get_tasks(start_date: date | None = None,
-              status: str | None = None,
-              access_token: str | None = Header(None)) -> TasksResponse:
+def get_tasks(
+    start_date: date | None = None,
+    status: str | None = None,
+    access_token: str | None = Header(None),
+) -> TasksResponse:
     valid_access_token(access_token)
     status_list: list[str] = status.split(",") if status else []
-    tasks = task.fetch_tasks(start_date=start_date,
-                             status_list=status_list)
+    tasks = task.fetch_tasks(start_date=start_date, status_list=status_list)
     return TasksResponse(data=[Task(t) for t in tasks])
+
 
 @router.get("/current", response_model=TasksResponse)
 def get_current_tasks(access_token: str | None = Header(None)) -> TasksResponse:
