41c6b3f444bbe1d3562e7615d176eb2a24c992cb
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-05-27 20:25:22 +0900

    feat: Add create_routine_task method to TaskFactory
    This commit adds a new method, create_routine_task, to the TaskFactory class in the notion_api/task/task_factory.py file. The method allows for the creation of RoutineTask objects with specified title, routine type, and optional due time. This enhancement improves the flexibility and functionality of the TaskFactory class.

--- notion_api/task/task_factory.py
+++ notion_api/task/task_factory.py
@@ -2,8 +2,11 @@ from datetime import date, datetime
 
 from notion_client_wrapper.block.block import Block
 from notion_client_wrapper.properties.properties import Properties
+from notion_client_wrapper.properties.text import Text
 from notion_client_wrapper.properties.title import Title
 from task.domain.due_date import DueDate
+from task.domain.routine_kind import RoutineKind, RoutineType
+from task.domain.routine_task import RoutineTask
 from task.domain.task import Task
 from task.domain.task_kind import TaskKind, TaskKindType
 from task.domain.task_start_date import TaskStartDate
@@ -34,3 +37,17 @@ class TaskFactory:
         if status is not None:
             properties.append(TaskStatus.from_status_type(status))
         return Task(properties=Properties(values=properties), block_children=blocks)
+
+    @classmethod
+    def create_routine_task(
+        cls: "TaskFactory",
+        title: str,
+        routine_type: RoutineType,
+        due_time: str | None = None,
+    ) -> Task:
+        title_property = Title.from_plain_text(text=title)
+        routine_kind = RoutineKind.create(routine_type)
+        properties = [title_property, routine_kind]
+        if due_time is not None:
+            properties.append(Text.from_plain_text(name="締め切り", text=due_time))
+        return RoutineTask(properties=Properties(values=properties), block_children=[])
