07b6f44d051ed54cb915ebc5a335c11304be99e8
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-05-30 00:38:02 +0900

    feat: Update task status update method to use "DONE" instead of "COMPLETED"

--- notion_api/usecase/task/complete_task_usecase.py
+++ notion_api/usecase/task/complete_task_usecase.py
@@ -10,5 +10,5 @@ class CompleteTaskUsecase:
     def execute(self, task_id: str) -> Task:
         task = self._task_repository.find_by_id(task_id=task_id)
         if task is not None:
-            task.update_status(TaskStatusType.COMPLETED)
+            task.update_status(TaskStatusType.DONE)
         return self._task_repository.save(task)
