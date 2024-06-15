efa75a5127d7ea6e193bcca529c2d64fa3b7bc82
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-06-15 19:46:30 +0900

    feat: 0時0分の場合は時刻を含んでいないとみなす

--- notion_api/util/datetime.py
+++ notion_api/util/datetime.py
@@ -1,6 +1,29 @@
 from datetime import date, datetime, timedelta, timezone
+from enum import Enum
 
 JST = timezone(timedelta(hours=+9), "JST")
+
+LENGTH_DATE = 10  # "YYYY-MM-DD"
+
+
+class DateType(Enum):
+    DATE = "date"
+    DATETIME = "datetime"
+    NONE = "none"
+
+    @staticmethod
+    def get_datetype(value: date | datetime | None) -> "DateType":
+        if value is None:
+            return DateType.NONE
+        try:
+            # それぞれ変換できることを確認してから返却する
+            if len(value) == LENGTH_DATE:
+                date.fromisoformat(value)
+                return DateType.DATE
+            datetime.fromisoformat(value)
+            return DateType.DATETIME
+        except ValueError:
+            return DateType.NONE
 
 
 def jst_now() -> datetime:
@@ -16,20 +39,17 @@ def jst_today() -> date:
 
 
 def convert_to_date_or_datetime(value: str | None, cls: type | None = None) -> date | datetime | None:
-    if value is None:
-        return None
-    length_date = 10  # "YYYY-MM-DD"
-    value_error_msg = f"Invalid class: {cls}"
-    if len(value) == length_date:
-        tmp_date = date.fromisoformat(value)
-        if cls is None or cls == date:
-            return tmp_date
-        if cls == datetime:
-            return datetime(tmp_date.year, tmp_date.month, tmp_date.day, tzinfo=JST)
-        raise ValueError(value_error_msg)
-    tmp_datetime = datetime.fromisoformat(value)
-    if cls is None or cls == datetime:
-        return tmp_datetime
-    if cls == date:
-        return tmp_datetime.date()
-    raise ValueError(value_error_msg)
+    date_type = DateType.get_datetype(value)
+    match date_type:
+        case DateType.DATE:
+            tmp_date = date.fromisoformat(value)
+            return datetime(tmp_date.year, tmp_date.month, tmp_date.day, tzinfo=JST) if cls == datetime else tmp_date
+        case DateType.DATETIME:
+            tmp_datetime = datetime.fromisoformat(value)
+            return (
+                tmp_datetime.date()
+                if cls == date or (tmp_datetime.hour == 0 and tmp_datetime.minute == 0)
+                else tmp_datetime
+            )
+        case DateType.NONE:
+            return None
