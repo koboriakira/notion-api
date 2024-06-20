c0c482aa782300e817af63899357e7adf4130103
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-06-20 13:07:27 +0900

    リクエスト例を記載

--- notion_api/create_page.py
+++ notion_api/create_page.py
@@ -11,12 +11,13 @@ logging.basicConfig(level=logging.INFO)
 if Environment.is_dev():
     logging.basicConfig(level=logging.DEBUG)
 
+
 def execute(request: CreatePageRequest) -> dict:
     usecase = Injector.create_page_use_case()
     return usecase.execute(request=request)
 
 
-def handler(event:dict, context:dict) -> dict:  # noqa: ARG001
+def handler(event: dict, context: dict) -> dict:  # noqa: ARG001
     body: dict = json.loads(event["Records"][0]["body"])
     print("body", body)
     request = CreatePageRequest.from_params(body["params"])
@@ -27,13 +28,25 @@ def handler(event:dict, context:dict) -> dict:  # noqa: ARG001
         ErrorReporter().execute(
             slack_channel=request.slack_channel,
             slack_thread_ts=request.slack_thread_ts,
-    )
+        )
+
 
 if __name__ == "__main__":
     # python -m notion_api.create_page
-    request = CreatePageRequest(
-        url="https://tabelog.com/tokyo/A1305/A130501/13242384/",
-        title="コイティー サンシャインシティアルパ店",
+    # Restaurantのサンプル
+    # request = CreatePageRequest(
+    #     url="https://tabelog.com/tokyo/A1305/A130501/13242384/",
+    #     title="コイティー サンシャインシティアルパ店",
+    # )
 
+    # Spotifyのサンプル
+    request = CreatePageRequest(
+        url="https://open.spotify.com/track/1x99RdrXhjrOwxnumMsBxP",
+        title="君といつまでも - together forever mix",
+        cover="https://i.scdn.co/image/ab67616d0000b273f8ae712f93a9553ed84a8d1c",
+        params={
+            "artists": ["ECD", "DJ Mitsu The Beats"],
+            "release_date": "2017-03-08",
+        },
     )
     execute(request=request)
