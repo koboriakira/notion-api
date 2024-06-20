5a04294c84c66fa591fcc10fa18093eecaa45149
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-06-21 02:24:07 +0900

    feat: Add get_user_tweets method to LambdaTwitterApi class

--- notion_api/common/infrastructure/twitter/lambda_twitter_api.py
+++ notion_api/common/infrastructure/twitter/lambda_twitter_api.py
@@ -1,5 +1,6 @@
 import json
 import os
+from datetime import datetime
 
 import requests
 
@@ -8,6 +9,7 @@ from common.infrastructure.twitter.tweet_response import TweetResponse
 
 class TwitterApiError(Exception):
     pass
+
 
 class LambdaTwitterApi:
     def __init__(self) -> None:
@@ -18,7 +20,17 @@ class LambdaTwitterApi:
         response = self.__get(path=path)
         return TweetResponse.from_dict(response)
 
-    def __get(self, path: str, params: dict|None = None) -> None:
+    def get_user_tweets(
+        self,
+        user_screen_name: str,
+        start_datetime: datetime | None = None,
+        end_datetime: datetime | None = None,
+    ) -> list[TweetResponse]:
+        path = f"/user/{user_screen_name}/tweets/"
+        response = self.__get(path=path)
+        return [TweetResponse.from_dict(tweet) for tweet in response]
+
+    def __get(self, path: str, params: dict | None = None) -> None:
         url = f"{self.domain}{path}"
         response = requests.get(url, params=params, timeout=30)
         if response.status_code != 200:
@@ -31,5 +43,7 @@ class LambdaTwitterApi:
 if __name__ == "__main__":
     # python -m notion_api.common.infrastructure.twitter.lambda_twitter_api
     api = LambdaTwitterApi()
-    tweet = api.get_tweet("1772383862393348494")
-    print(tweet.to_entity())
+    # tweet = api.get_tweet("1772383862393348494")
+    # print(tweet.to_entity())
+    # tweets = api.get_user_tweets(user_screen_name="kobori_akira_pw")
+    # print(tweets[0].to_entity())
