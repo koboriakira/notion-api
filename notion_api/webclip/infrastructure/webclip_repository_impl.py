3842fed2616e97a9465c5ef929701577e0310968
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-07-25 10:32:16 +0900

    feat: Add lazy loading for restaurant cover image in RestaurantCreator

--- notion_api/webclip/infrastructure/webclip_repository_impl.py
+++ notion_api/webclip/infrastructure/webclip_repository_impl.py
@@ -8,17 +8,14 @@ from webclip.domain.webclip import Webclip
 
 
 class WebclipRepositoryImpl:
-    def __init__(
-            self,
-            client: ClientWrapper,
-            logger: Logger|None = None) -> None :
+    def __init__(self, client: ClientWrapper, logger: Logger | None = None) -> None:
         self._client = client
         self._logger = logger or getLogger(__name__)
 
-    def find_by_title(self, title: str) -> Webclip|None:
+    def find_by_title(self, title: str) -> Webclip | None:
         title_property = Title.from_plain_text(name="名前", text=title)
         filter_param = FilterBuilder.build_simple_equal_condition(title_property)
-        searched_webclips = self._client.retrieve_database(
+        searched_webclips: list[Webclip] = self._client.retrieve_database(
             database_id=DatabaseType.WEBCLIP.value,
             filter_param=filter_param,
             page_model=Webclip,
@@ -34,6 +31,10 @@ class WebclipRepositoryImpl:
         result = self._client.create_page_in_database(
             database_id=DatabaseType.WEBCLIP.value,
             properties=webclip.properties.values,
+        )
+        self._client.append_blocks(
+            block_id=result["id"],
+            blocks=webclip.block_children,
         )
         webclip.update_id_and_url(
             page_id=result["id"],
