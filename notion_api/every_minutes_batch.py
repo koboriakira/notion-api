94ef426618b9509922e58c3da4dace41d28e3bf8
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-08-09 01:09:20 +0900

    feat: Reset shopping list buy status in every_minutes_batch.py
    This commit adds the ResetShoppingListUseCase to the every_minutes_batch.py file. The ResetShoppingListUseCase is responsible for resetting the buy status of shopping items in the shopping list. It fetches all shopping items from the repository and checks if their buy status is set to DONE. If so, it resets the buy status and saves the updated shopping item back to the repository. This feature improves the functionality of the shopping list by allowing users to easily reset completed shopping items.

--- notion_api/every_minutes_batch.py
+++ notion_api/every_minutes_batch.py
@@ -2,8 +2,10 @@ import logging
 
 from custom_logger import get_logger
 from notion_client_wrapper.client_wrapper import ClientWrapper
+from shopping.infrastructure.repository_impl import ShoppingRepositoryImpl
 from task.infrastructure.task_repository_impl import TaskRepositoryImpl
 from usecase.clean_empty_title_page import CleanEmptyTitlePageUsecase
+from usecase.shopping.reset_shopping_list_usecase import ResetShoppingListUseCase
 from usecase.task.do_tomorrow_usecase import DoTommorowUsecase
 from util.environment import Environment
 from util.error_reporter import ErrorReporter
@@ -13,7 +15,8 @@ client = ClientWrapper.get_instance(logger=logger)
 clean_empty_title_page_usecase = CleanEmptyTitlePageUsecase(client=client, logger=logger)
 task_repository = TaskRepositoryImpl(notion_client_wrapper=client)
 do_tomorrow_usecase = DoTommorowUsecase(task_repository=task_repository)
-
+shopping_repository = ShoppingRepositoryImpl(client)
+reset_shopping_list_usecase = ResetShoppingListUseCase(shopping_repository)
 
 # ログ
 logging.basicConfig(level=logging.INFO)
@@ -27,6 +30,9 @@ def handler(event: dict, context: dict) -> None:
         clean_empty_title_page_usecase.handle()
         logger.info("「明日やる」が有効になっているタスクを翌日に更新")
         do_tomorrow_usecase.execute()
+        logger.info("買い物リストの購入ステータスをリセット")
+        reset_shopping_list_usecase.execute()
+
     except:
         ErrorReporter().execute()
         raise
--- notion_api/usecase/shopping/reset_shopping_list_usecase.py
+++ notion_api/usecase/shopping/reset_shopping_list_usecase.py
@@ -1,15 +1,19 @@
+from logging import Logger, getLogger
+
 from shopping.domain.buy_status import BuyStatusType
 from shopping.domain.repository import ShoppingRepository
 
 
 class ResetShoppingListUseCase:
-    def __init__(self, shopping_repository: ShoppingRepository) -> None:
+    def __init__(self, shopping_repository: ShoppingRepository, logger: Logger | None = None) -> None:
         self._shopping_repository = shopping_repository
+        self._logger = logger or getLogger(__name__)
 
     def execute(self) -> None:
         all_shopping_list = self._shopping_repository.fetch_all()
         for shopping in all_shopping_list:
             if shopping.buy_status == BuyStatusType.DONE:
+                self._logger.info(f"Reset buy status: {shopping.name}")
                 reseted_shopping = shopping.reset_buy_status_type()
                 self._shopping_repository.save(reseted_shopping)
 
