01c67905246ff954ba5c48c41286b67423580c9a
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-11-16 01:03:30 +0900

    Refactor blog template router: clean up imports and improve code formatting

--- notion_api/router/blog.py
+++ notion_api/router/blog.py
@@ -1,16 +1,19 @@
-from datetime import datetime, timedelta
-from fastapi import APIRouter, Header
 import logging
+from datetime import datetime
+
+from fastapi import APIRouter
+
 from injector.injector import Injector
 from router.response import BaseResponse
 from util.date_range import DateRange
-from util.datetime import JST, jst_now
+from util.datetime import jst_now
 from util.dynamodb.dynamodb import DynamoDBClient
 
 router = APIRouter()
 
-KEY = 'latest-datetime-getting-blog-template'
-dynamodb_client = DynamoDBClient('NotionApi-NotionTableF26AF3BC-4263X0XU1OXU')
+KEY = "latest-datetime-getting-blog-template"
+dynamodb_client = DynamoDBClient("NotionApi-NotionTableF26AF3BC-4263X0XU1OXU")
+
 
 @router.get("/template/", response_model=BaseResponse)
 def get_blog_template(last_execution_time: bool | None = None) -> BaseResponse:
@@ -18,16 +21,16 @@ def get_blog_template(last_execution_time: bool | None = None) -> BaseResponse:
     ブログのテンプレート文章を返却する
     """
     try:
-        start_str = dynamodb_client.find('key', KEY)['datetime']
+        start_str = dynamodb_client.find("key", KEY)["datetime"]
         start = datetime.fromisoformat(start_str)
         now = jst_now()
 
         usecase = Injector.create_collect_updated_pages_usecase()
         markdown_text = usecase.execute(
-            date_range=DateRange.from_datetime(start=start, end=now)
-            )
+            date_range=DateRange.from_datetime(start=start, end=now),
+        )
 
-        # dynamodb_client.put({'key': KEY, 'datetime': now.isoformat()})
+        dynamodb_client.put({"key": KEY, "datetime": now.isoformat()})
         return BaseResponse(message=markdown_text)
     except Exception as e:
         logging.error(e)
