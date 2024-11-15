0322a272bfa8fd5ac27bf5de1a58cd936cb75d62
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-11-16 03:14:20 +0900

    feat(webclip): implement WebclipRepository and its implementation

--- notion_api/webclip/domain/webclip.py
+++ notion_api/webclip/domain/webclip.py
@@ -16,12 +16,13 @@ COLUMN_NAME_TITLE = "名前"
 class Webclip(BasePage):
     @staticmethod
     def create(
-            title: str|Title,
-            url: str|Url,
-            tag_relation: list[str]|TagRelation|None = None,
-            summary: str|Summary|None = None,
-            blocks: list[Block]|None = None,
-            cover: str|Cover|None = None) -> "Webclip":
+        title: str | Title,
+        url: str | Url,
+        tag_relation: list[str] | TagRelation | None = None,
+        summary: str | Summary | None = None,
+        blocks: list[Block] | None = None,
+        cover: str | Cover | None = None,
+    ) -> "Webclip":
         blocks = blocks or []
         properties = [
             title if isinstance(title, Title) else Title.from_plain_text(name=COLUMN_NAME_TITLE, text=title),
@@ -31,7 +32,7 @@ class Webclip(BasePage):
             if isinstance(tag_relation, TagRelation):
                 properties.append(tag_relation)
             else:
-              properties.append(TagRelation.from_id_list(id_list=tag_relation))
+                properties.append(TagRelation.from_id_list(id_list=tag_relation))
         if summary is not None:
             if isinstance(summary, Summary):
                 properties.append(summary)
@@ -42,3 +43,7 @@ class Webclip(BasePage):
             return Webclip(properties=Properties(values=properties), block_children=blocks)
         cover = cover if isinstance(cover, Cover) else Cover.from_external_url(cover)
         return Webclip(properties=Properties(values=properties), block_children=blocks, cover=cover)
+
+    @property
+    def cliped_url(self) -> str:
+        return self.get_url("URL").url
