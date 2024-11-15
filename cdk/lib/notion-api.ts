7452f465efd8810d03502348d9142c537d9fe17c
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-11-15 17:19:16 +0900

    Add tableName property to NotionTable DynamoDB creation

--- cdk/lib/notion-api.ts
+++ cdk/lib/notion-api.ts
@@ -32,6 +32,7 @@ export class NotionApi extends Stack {
 
     // dynamoDBの作成
     const table = new dynamodb.Table(this, "NotionTable", {
+      tableName: "NotionTable",
       partitionKey: {
         name: "key",
         type: dynamodb.AttributeType.STRING,
