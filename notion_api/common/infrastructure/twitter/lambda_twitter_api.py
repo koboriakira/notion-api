23086c6538a069801403050271e5c9d89436d45b
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-11-15 17:01:51 +0900

    ユーザーのツイート取得機能を更新し、開始日時と終了日時をパラメータとして追加。APIリクエストのタイムアウトを30秒から180秒に延長。

--- notion_api/common/infrastructure/twitter/lambda_twitter_api.py
+++ notion_api/common/infrastructure/twitter/lambda_twitter_api.py
@@ -1,11 +1,12 @@
 import json
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import requests
 
 import custom_logger
 from common.infrastructure.twitter.tweet_response import TweetModel
+from util.datetime import jst_now
 
 
 class TwitterApiError(Exception):
@@ -29,14 +30,18 @@ class LambdaTwitterApi:
         end_datetime: datetime | None = None,
     ) -> list[TweetModel]:
         path = f"/user/{user_screen_name}/tweets/"
-        response = self.__get(path=path)
+        params = {
+            "start": start_datetime.isoformat() if start_datetime else None,
+            "end": end_datetime.isoformat() if end_datetime else None,
+        }
+        response = self.__get(path=path, params=params)
         self._logger.debug(f"response: {response['data']}")
         response_data: list[dict] = response["data"]
         return [TweetModel.from_dict(tweet) for tweet in response_data]
 
     def __get(self, path: str, params: dict | None = None) -> dict:
         url = f"{self.domain}{path}"
-        response = requests.get(url, params=params, timeout=30)
+        response = requests.get(url, params=params, timeout=180)
         if response.status_code != 200:
             data_str = json.dumps(params, ensure_ascii=False)
             error_messsage = f"Failed to get to {url}. status_code: {response.status_code}, response: {response.text}, params: {data_str}"
@@ -49,5 +54,11 @@ if __name__ == "__main__":
     api = LambdaTwitterApi()
     # tweet = api.get_tweet("1772269440396333105")
     # print(tweet.to_entity())
-    tweets = api.get_user_tweets(user_screen_name="kobori_akira_pw")
+    now = jst_now()
+    tweets = api.get_user_tweets(
+        user_screen_name="kobori_akira_pw",
+        start_datetime=now - timedelta(hours=7),
+        end_datetime=now,
+        )
+    print(len(tweets))
     print(tweets[0].to_entity())
