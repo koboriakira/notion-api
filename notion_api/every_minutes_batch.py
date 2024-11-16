1cf6adf7e3ff0e361cabfbbf3f94ecb4c90cb9e9
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-11-16 11:54:57 +0900

    feat: プロジェクト関連のタスクは自動的にタスク種別をつける

--- notion_api/every_minutes_batch.py
+++ notion_api/every_minutes_batch.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import timedelta
 
 from custom_logger import get_logger
 from notion_client_wrapper.client_wrapper import ClientWrapper
@@ -8,6 +9,7 @@ from usecase.clean_empty_title_page import CleanEmptyTitlePageUsecase
 from usecase.shopping.reset_shopping_list_usecase import ResetShoppingListUseCase
 from usecase.task.maintain_tasks_usecase import MaintainTasksUsecase
 from usecase.task.start_task_usecase import StartTaskUsecase
+from util.datetime import jst_now
 from util.environment import Environment
 from util.error_reporter import ErrorReporter
 
@@ -15,7 +17,7 @@ logger = get_logger(__name__)
 client = ClientWrapper.get_instance(logger=logger)
 clean_empty_title_page_usecase = CleanEmptyTitlePageUsecase(client=client, logger=logger)
 task_repository = TaskRepositoryImpl(notion_client_wrapper=client)
-do_tomorrow_usecase = MaintainTasksUsecase(task_repository=task_repository)
+maintain_tasks_usecase = MaintainTasksUsecase(task_repository=task_repository)
 shopping_repository = ShoppingRepositoryImpl(client)
 reset_shopping_list_usecase = ResetShoppingListUseCase(shopping_repository)
 start_task_usecase = StartTaskUsecase(task_repository=task_repository)
@@ -28,12 +30,14 @@ if Environment.is_dev():
 
 def handler(event: dict, context: dict) -> None:
     try:
+        logger.info("更新されたタスクに関して自動調整")
+        # FIXME: 前回のバッチ実行開始時刻にしたいけど、とりあえずこれで
+        latest_edited_at = jst_now() - timedelta(minutes=10)
+        maintain_tasks_usecase.execute(latest_edited_at)
         logger.info("「開始」が有効になっているタスクについて開始処理をする")
         start_tasks()
         logger.info("タイトルが空のページを削除")
         clean_empty_title_page_usecase.handle()
-        logger.info("「明日やる」が有効になっているタスクを翌日に更新")
-        do_tomorrow_usecase.execute()
         logger.info("買い物リストの購入ステータスをリセット")
         reset_shopping_list_usecase.execute()
 
--- notion_api/usecase/task/maintain_tasks_usecase.py
+++ notion_api/usecase/task/maintain_tasks_usecase.py
@@ -1,23 +1,34 @@
+from datetime import datetime, timedelta
+from logging import Logger
+
+from custom_logger import get_logger
+from task.domain.task_kind import TaskKindType
 from task.domain.task_repository import TaskRepository
+from util.datetime import jst_now
 
 
 class MaintainTasksUsecase:
-    def __init__(self, task_repository: TaskRepository) -> None:
+    def __init__(self, task_repository: TaskRepository, logger: Logger | None = None) -> None:
         self._task_repository = task_repository
+        self._logger = logger or get_logger(__name__)
 
-    def execute(self) -> None:
-        tasks = self._task_repository.search(do_tomorrow_flag=True)
+    def execute(self, last_edited_at: datetime) -> None:
+        tasks = self._task_repository.search(last_edited_at=last_edited_at)
+
         for task in tasks:
-            self._task_repository.save(task=task.do_tomorrow())
+            if task.is_do_tomorrow:
+                self._logger.info(f"「明日やる」タスクを処理: {task.title}")  # noqa: G004
+                self._task_repository.save(task=task.do_tomorrow())
+            if len(task.project_id_list) > 0 and task.kind is None:
+                self._logger.info(f"タスク種別のないプロジェクト関連タスクを処理: {task.title}")  # noqa: G004
+                self._task_repository.save(task=task.update_kind(TaskKindType.NEXT_ACTION))
 
 
 if __name__ == "__main__":
-    # python -m notion_api.usecase.task.do_tomorrow_usecase
-    from custom_logger import get_logger
+    # python -m notion_api.usecase.task.maintain_tasks_usecase
     from task.infrastructure.task_repository_impl import TaskRepositoryImpl
-
-    logger = get_logger(__name__)
 
     task_repository = TaskRepositoryImpl()
     usecase = MaintainTasksUsecase(task_repository=task_repository)
-    usecase.execute()
+    last_edited_at = jst_now() - timedelta(minutes=30)
+    usecase.execute(last_edited_at=last_edited_at)
