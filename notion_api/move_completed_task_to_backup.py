e6effe6755969f5ecbd98a68efebd785b719addd
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-11-22 14:10:34 +0900

    fix: コンストラクタの指定漏れを修正

--- notion_api/move_completed_task_to_backup.py
+++ notion_api/move_completed_task_to_backup.py
@@ -1,6 +1,7 @@
 import logging
 
 from notion_client_wrapper.client_wrapper import ClientWrapper
+from project.infrastructure.project_repository_impl import ProjectRepositoryImpl
 from task.infrastructure.task_repository_impl import TaskRepositoryImpl
 from usecase.move_tasks_to_backup_usecase import MoveTasksToBackupUsecase
 from util.environment import Environment
@@ -14,11 +15,12 @@ if Environment.is_dev():
 
 def handler(event: dict, context: dict) -> None:  # noqa: ARG001
     try:
-        task_repository = TaskRepositoryImpl(
-            notion_client_wrapper=ClientWrapper.get_instance(),
-        )
+        notion_client = ClientWrapper.get_instance()
+        task_repository = TaskRepositoryImpl(notion_client_wrapper=notion_client)
+        project_repository = ProjectRepositoryImpl(client=notion_client)
         usecase = MoveTasksToBackupUsecase(
             task_repository=task_repository,
+            project_repository=project_repository,
         )
         usecase.execute()
     except:
