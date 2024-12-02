f8afd034e527f937d1daea54a9b6a495ad940aa4
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-12-03 00:02:14 +0900

    feat: 外部カレンダーを同期する新しい機能を追加し、毎日20時に実行されるようにスケジュールを設定

---
+++ notion_api/sync_external_calendar.py
@@ -0,0 +1,29 @@
+
+from external_calendar.infrastructure.google_calendar_api import GoogleCalendarApi
+from external_calendar.service.external_calendar_service import ExternalCalendarService
+from task.infrastructure.task_repository_impl import TaskRepositoryImpl
+from usecase.task.sync_external_calendar_usecase import SyncExternalCalendarUsecase
+from util.datetime import jst_now
+from util.error_reporter import ErrorReporter
+
+
+def handler(event: dict, context: dict) -> None:
+    try:
+        task_repository = TaskRepositoryImpl()
+        external_calendar_service = ExternalCalendarService(
+            api=GoogleCalendarApi(),
+        )
+        suite = SyncExternalCalendarUsecase(
+            task_repository=task_repository,
+            external_calendar_service=external_calendar_service,
+        )
+        print(suite.execute(date_=jst_now().date()))
+
+    except Exception as e:
+        ErrorReporter().execute(error=e)
+        raise
+
+
+if __name__ == "__main__":
+    # python -m notion_api.every_day_batch
+    handler({}, {})
