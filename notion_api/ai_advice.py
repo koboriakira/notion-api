32efc2470556e046cf407bea55f93172e11bfa00
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-12-19 05:12:28 +0900

    feat: AIアドバイス機能を実装

---
+++ notion_api/ai_advice.py
@@ -0,0 +1,30 @@
+import logging
+
+from task.infrastructure.task_repository_impl import TaskRepositoryImpl
+from usecase.ai_advice_usecase import AiAdviceUsecase
+from util.datetime import jst_now
+from util.environment import Environment
+from util.error_reporter import ErrorReporter
+
+task_repository = TaskRepositoryImpl()
+suite = AiAdviceUsecase(
+    task_repository=task_repository,
+)
+
+# ログ
+logging.basicConfig(level=logging.INFO)
+if Environment.is_dev():
+    logging.basicConfig(level=logging.DEBUG)
+
+
+def handler(event: dict, context: dict) -> None:  # noqa: ARG001
+    try:
+        suite.execute(start_datetime=jst_now())
+    except Exception as e:
+        ErrorReporter().execute(error=e)
+        raise
+
+
+if __name__ == "__main__":
+    # python -m notion_api.ai_advice
+    handler({}, {})
