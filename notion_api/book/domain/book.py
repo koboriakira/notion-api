81699a16bbe91f05c264bc98015a1250882a8fd1
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-12-20 00:34:46 +0900

    feat: ISBNを取得する関数を実装

--- notion_api/book/domain/book.py
+++ notion_api/book/domain/book.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass
 from datetime import date
 
+from lotion.base_page import BasePage
+from lotion.page.page_id import PageId
+
 from book.domain.authors import Authors
 from book.domain.book_url import BookUrl
 from book.domain.published_date import PublishedDate
 from book.domain.publisher import Publisher
-from lotion.base_page import BasePage
-from lotion.page.page_id import PageId
 
 
 @dataclass
@@ -29,3 +30,24 @@ class Book(BasePage):
     def book_url(self) -> str:
         url = self.get_url(name=BookUrl.NAME)
         return url.url if url else ""
+
+    @property
+    def isbn(self) -> str:
+        return self.get_text(name="ISBN").text
+
+    @staticmethod
+    def cast(base_page: BasePage) -> "Book":
+        return Book(
+            properties=base_page.properties,
+            block_children=base_page.block_children,
+            id_=base_page.id_,
+            url=base_page.url,
+            created_time=base_page.created_time,
+            last_edited_time=base_page.last_edited_time,
+            _created_by=base_page._created_by,
+            _last_edited_by=base_page._last_edited_by,
+            cover=base_page.cover,
+            icon=base_page.icon,
+            archived=base_page.archived,
+            parent=base_page.parent,
+        )
