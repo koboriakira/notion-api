5c5ceb139b02f843531d3c5101af4cbfbc8013bb
Kobori Akira <private.beats@gmail.com>
2024-12-19 21:09:48 +0900

    feat: 受け取ったリクエストをページエンティティに変換

--- notion_api/router/notion_webhook.py
+++ notion_api/router/notion_webhook.py
@@ -1,12 +1,14 @@
 import json
 import logging
-from typing import Any
 
 from fastapi import APIRouter
+from lotion import BasePage
+from pydantic import BaseModel
 
 from router.response import BaseResponse
 
 router = APIRouter()
+
 
 @router.get("/")
 def get() -> BaseResponse:
@@ -22,17 +24,26 @@ def get() -> BaseResponse:
         print(e)
         return BaseResponse(message="Error", data=e)
 
+
+class NotionWebhookRequest(BaseModel):
+    source: dict
+    data: dict
+
+
 @router.post("/")
-def post(data: Any) -> BaseResponse:
+def post(request: NotionWebhookRequest) -> BaseResponse:
     """
     Return a greeting
     """
     try:
-        print("notion_webhook: get")
-        logging.debug("notion_webhook: get")
-        logging.info("notion_webhook: get")
-        print(data)
-        print(json.dumps(data, ensure_ascii=False))
+        print("notion_webhook: post")
+        logging.debug("notion_webhook: post")
+        logging.info("notion_webhook: post")
+        print(request)
+        print(request.data)
+        print(json.dumps(request.data, ensure_ascii=False))
+        base_page = BasePage.from_data(request.data)
+        print(base_page.page_id.value)
         return BaseResponse()
     except Exception as e:
         print(e)
