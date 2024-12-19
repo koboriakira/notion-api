0b6729372c259ff91dd57df3cf0dfdf55a6c9fc5
Kobori Akira <private.beats@gmail.com>
2024-12-19 14:43:16 +0900

    feat: エラーを取り扱う

--- notion_api/router/notion_webhook.py
+++ notion_api/router/notion_webhook.py
@@ -13,20 +13,27 @@ def get() -> BaseResponse:
     """
     Return a greeting
     """
-    print("notion_webhook: get")
-    logging.debug("notion_webhook: get")
-    logging.info("notion_webhook: get")
-    return BaseResponse(message="Healthcheck OK")
-
+    try:
+        print("notion_webhook: get")
+        logging.debug("notion_webhook: get")
+        logging.info("notion_webhook: get")
+        return BaseResponse()
+    except Exception as e:
+        print(e)
+        return BaseResponse(message="Error", data=e)
 
 @router.post("/")
 def post(data: Any) -> BaseResponse:
     """
     Return a greeting
     """
-    print("notion_webhook: get")
-    logging.debug("notion_webhook: get")
-    logging.info("notion_webhook: get")
-    print(data)
-    print(json.dumps(data, ensure_ascii=False))
-    return BaseResponse(message="Healthcheck OK")
+    try:
+        print("notion_webhook: get")
+        logging.debug("notion_webhook: get")
+        logging.info("notion_webhook: get")
+        print(data)
+        print(json.dumps(data, ensure_ascii=False))
+        return BaseResponse()
+    except Exception as e:
+        print(e)
+        return BaseResponse(message="Error", data=e)
