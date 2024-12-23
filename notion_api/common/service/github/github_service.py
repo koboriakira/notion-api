0d995f113fcfcfb5b3105bf94f61a24d9cb7a7fd
Kobori Akira <private.beats@gmail.com>
2024-12-23 14:25:45 +0900

    feat: GitHub APIを利用するサービスを作成

---
+++ notion_api/common/service/github/github_service.py
@@ -0,0 +1,20 @@
+from lotion.lotion import os
+
+
+from github import Github, Auth
+
+class GitHubService:
+    def __init__(self, github_access_token: str | None = None) -> None:
+        access_token = os.environ["GITHUB_ACCESS_TOKEN"] if github_access_token is None else github_access_token
+        auth = Auth.Token(access_token)
+        self._pygithub = Github(auth=auth)
+
+
+    def get_repos(self):
+        # Then play with your Github objects:
+        for repo in self._pygithub.get_user().get_repos():
+            print(repo.name)
+
+if __name__ == "__main__":
+    service = GitHubService()
+    service.get_repos()
