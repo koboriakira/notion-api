43adba825a60f5129e76b3ee7990cce06698264c
Akira Kobori <private.beats@gmail.com>
2024-12-26 02:37:09 +0900

    feat: PJ化のボタンを作成 (#59)
    * Create empty commit for PR
    
    * feat: PJ化のWebhookを作成
    
    ---------
    
    Co-authored-by: a-kobori <89172056+a-kobori@users.noreply.github.com>

---
+++ notion_api/usecase/project/convert_to_project_usecase.py
@@ -0,0 +1,19 @@
+from lotion import Lotion
+from lotion.properties import Properties, Title
+
+from custom_logger import get_logger
+from project.domain.project import Project
+from project.domain.project_repository import ProjectRepository
+
+logger = get_logger(__name__)
+
+
+class ConvertToProjectUsecase:
+    def __init__(self, lotion: Lotion, project_repository: ProjectRepository) -> None:
+        self._lotion = lotion
+        self._project_repository = project_repository
+
+    def execute(self, page_id: str, title: Title) -> Project:
+        project = self._project_repository.save(Project(Properties([title]), block_children=[]))
+        self._lotion.remove_page(page_id=page_id)
+        return project
