4cf58b02f5b8867c04a4d0b8ce98fddae05463be
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-12-27 21:34:38 +0900

    chore: 利用しないコードを整理

--- notion_api/account_book/domain/category.py
+++ notion_api/account_book/domain/category.py
@@ -1,14 +1,5 @@
 from enum import Enum
 
-kind_map = {
-    "住居・水道光熱・通信費": {"selected_id": "f3a44bbf-234e-494c-8fdf-3269475da426", "selected_color": "blue"},
-    "雑費": {"selected_id": "cd0c84d5-d959-4f06-a929-ec780a7c2d3d", "selected_color": "default"},
-    "特別費(東京女子プロレス)": {"selected_id": "19af0797-6750-40e8-a885-1c84d9425803", "selected_color": "green"},
-    "趣味・交際費": {"selected_id": "8473f30c-f27c-4fe0-a827-f813c9b9197b", "selected_color": "red"},
-    "被服・美容費": {"selected_id": "684dcc6f-ca0b-492b-a9b5-a5096d421a74", "selected_color": "gray"},
-    "食費・日用品・生活費": {"selected_id": "5493b7c1-a43d-4f91-89b6-94869393cf85", "selected_color": "purple"},
-}
-
 
 class CategoryType(Enum):
     RESIDENCE_UTILITIES_COMMUNICATIONS = "住居・水道光熱・通信費"
@@ -25,27 +16,3 @@ class CategoryType(Enum):
                 return kind_type
         msg = f"CategoryType に存在しない値です: {text}"
         raise ValueError(msg)
-
-    @property
-    def priority(self) -> int:
-        return {
-            CategoryType.TRASH: 0,
-            CategoryType.ROUTINE: 1,
-            CategoryType.WAIT: 2,
-            CategoryType.SOMEDAY_MAYBE: 3,
-            CategoryType.SCHEDULE: 4,
-            CategoryType.NEXT_ACTION: 5,
-            CategoryType.DO_NOW: 6,
-        }[self]
-
-    @property
-    def selected_name(self) -> str:
-        return self.value
-
-    @property
-    def selected_id(self) -> str:
-        return kind_map[self.value]["selected_id"]
-
-    @property
-    def selected_color(self) -> str:
-        return kind_map[self.value]["selected_color"]
--- notion_api/account_book/domain/tag.py
+++ notion_api/account_book/domain/tag.py
@@ -1,41 +1,13 @@
-from dataclasses import dataclass
-
-from lotion.properties.multi_select import MultiSelectElement
-
-KIND_LIST = [{"name": "music bar t", "id": "53bf9fc9-7e08-42d0-89fa-e67c224ba921"}]
+from enum import Enum
 
 
-@dataclass(frozen=True)
-class TagType:
-    name: str
-    id: str
+class AccountTag(Enum):
+    MUSIC_BAR_T = "music bar t"
 
     @staticmethod
-    def from_text(text: str) -> "TagType":
-        for kind_type in KIND_LIST:
-            if kind_type["name"] == text:
-                return TagType(**kind_type)
-        msg = f"TagType に存在しない値です: {text}"
+    def from_text(text: str) -> "AccountTag":
+        for kind_type in AccountTag:
+            if kind_type.value == text:
+                return kind_type
+        msg = f"AccountTag に存在しない値です: {text}"
         raise ValueError(msg)
-
-    def __dict__(self) -> dict[str, str]:
-        return {"name": self.name, "id": self.id}
-
-    def __eq__(self, __value: object) -> bool:
-        if not isinstance(__value, TagType):
-            return False
-        return self.id == __value.id
-
-
-@dataclass(frozen=True)
-class TagTypes:
-    values: list[TagType]
-
-    def __post_init__(self) -> None:
-        for value in self.values:
-            if not isinstance(value, TagType):
-                msg = f"[{type(self)} Invalid type for {value}: {type(value)}"
-                raise TypeError(msg)
-
-    def to_multi_select_elements(self) -> list[MultiSelectElement]:
-        return [MultiSelectElement(**kind.__dict__()) for kind in self.values]
