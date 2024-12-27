1f6b014b4e33d9d7611d15226aefc422214358fc
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-12-27 21:31:41 +0900

    refactor: simplify account book creation and response handling

--- notion_api/usecase/account_book/add_account_book_usecase.py
+++ notion_api/usecase/account_book/add_account_book_usecase.py
@@ -4,8 +4,6 @@ from lotion import Lotion
 from lotion.properties import Properties
 
 from account_book.domain.account_book import AccountBook, AccountDate, AccountTitle, Category, IsFixedCost, Price, Tag
-from account_book.domain.category import CategoryType
-from account_book.domain.tag import TagType, TagTypes
 
 
 class AddAccountBookUsecase:
@@ -23,62 +21,24 @@ class AddAccountBookUsecase:
         category: str | None = None,
         tag: list[str] | None = None,
         date_: date | None = None,
-    ) -> dict[str, str]:
-        account_book = self._create_entity(
-            title=title,
-            price=price,
-            is_fixed_cost=is_fixed_cost,
-            category=category,
-            tag=tag,
-            date_=date_,
-        )
-        return self._save(account_book).get_id_and_url()
-
-    def _save(self, entity: AccountBook) -> AccountBook:
-        """Save a AccountBook item."""
-        # FIXME: `update`で自動的に作成or更新するようにしたい
-        if entity.is_created():
-            self._lotion.update_page(page_id=entity.id, properties=entity.properties.values)
-            return entity
-        return self._lotion.create_page(entity)
-
-    def _create_entity(  # noqa: PLR0913
-        self,
-        title: str,
-        price: int,
-        is_fixed_cost: bool | None = None,
-        category: str | None = None,
-        tag: list[str] | None = None,
-        date_: date | None = None,
     ) -> AccountBook:
-        properties = []
+        account_book = AccountBook(properties=Properties([]))
 
-        properties.append(AccountTitle.from_plain_text(text=title))
-        properties.append(Price.from_num(value=price))
+        account_book.set_prop(AccountTitle.from_plain_text(text=title))
+        account_book.set_prop(Price.from_num(value=price))
 
         if is_fixed_cost:
-            properties.append(IsFixedCost.true())
+            account_book.set_prop(IsFixedCost.true())
 
         if category:
-            category_type = CategoryType.from_text(text=category)
-            _category = Category(
-                name=Category.PROP_NAME,
-                selected_name=category_type.selected_name,
-                selected_id=category_type.selected_id,
-                selected_color=category_type.selected_color,
-                id=None,
-            )
-            properties.append(_category)
+            _category = self._lotion.fetch_select(AccountBook, Category, category)
+            account_book.set_prop(_category)
 
         if tag and len(tag) > 0:
-            tag_type_list = TagTypes([TagType.from_text(text=value) for value in tag])
-            _tag = Tag(
-                name=Tag.PROP_NAME,
-                values=tag_type_list.to_multi_select_elements(),
-            )
-            properties.append(_tag)
+            _tag = self._lotion.fetch_multi_select(AccountBook, Tag, tag)
+            account_book.set_prop(_tag)
 
         if date_:
-            properties.append(AccountDate.from_start_date(start_date=date_))
+            account_book.set_prop(AccountDate.from_start_date(start_date=date_))
 
-        return AccountBook(properties=Properties(values=properties))
+        return self._lotion.update(account_book)
