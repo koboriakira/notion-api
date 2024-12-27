70767ece31bfa20105ebef365ee195cfa8bba405
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-12-27 20:26:38 +0900

    chore: 新しいLotionライブラリを活用して実装量を削減

--- e2e/specs/accountBook.spec
+++ e2e/specs/accountBook.spec
@@ -2,5 +2,7 @@
 
 ## Add a new account book
 
+tags: current
+
 * Create a account book with title "テスト" and price "1000"
 * Delete the latest created page
--- notion_api/account_book/domain/account_book.py
+++ notion_api/account_book/domain/account_book.py
@@ -1,8 +1,45 @@
-from dataclasses import dataclass
-
+from lotion import notion_database, notion_prop
 from lotion.base_page import BasePage
+from lotion.properties import Checkbox, Date, MultiSelect, Number, Select, Title
 
 
-@dataclass
+@notion_prop("名前")
+class AccountTitle(Title):
+    pass
+
+
+@notion_prop("日付")
+class AccountDate(Date):
+    pass
+
+
+@notion_prop("金額")
+class Price(Number):
+    pass
+
+
+@notion_prop("固定費")
+class IsFixedCost(Checkbox):
+    pass
+
+
+@notion_prop("費目")
+class Category(Select):
+    pass
+
+
+@notion_prop("タグ")
+class Tag(MultiSelect):
+    pass
+
+
+@notion_database("f2c5fc6e-4c27-429f-add5-2279f1f84e8d")
 class AccountBook(BasePage):
     """家計簿クラス"""
+
+    account_title: AccountTitle
+    date: AccountDate
+    price: Price
+    is_fixed_cost: IsFixedCost
+    category: Category
+    tag: Tag
