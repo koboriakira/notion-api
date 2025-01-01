59b9e1ae601e7c54e1904bb7f02274ab2a892ecc
a-kobori <89172056+a-kobori@users.noreply.github.com>
2025-01-01 21:20:40 +0900

    fix: スケジュール開始の機能を修正

--- notion_api/every_minutes_batch.py
+++ notion_api/every_minutes_batch.py
@@ -1,5 +1,4 @@
 import logging
-from datetime import timedelta
 
 from lotion import Lotion
 
@@ -9,7 +8,6 @@ from usecase.clean_empty_title_page import CleanEmptyTitlePageUsecase
 from usecase.shopping.reset_shopping_list_usecase import ResetShoppingListUseCase
 from usecase.task.maintain_tasks_usecase import MaintainTasksUsecase
 from usecase.task.start_task_usecase import StartTaskUsecase
-from util.datetime import jst_now
 from util.environment import Environment
 from util.error_reporter import ErrorReporter
 
@@ -17,7 +15,7 @@ logger = get_logger(__name__)
 client = Lotion.get_instance(logger=logger)
 clean_empty_title_page_usecase = CleanEmptyTitlePageUsecase(client=client, logger=logger)
 task_repository = TaskRepositoryImpl(notion_client_wrapper=client)
-maintain_tasks_usecase = MaintainTasksUsecase(task_repository=task_repository)
+maintain_tasks_usecase = MaintainTasksUsecase()
 reset_shopping_list_usecase = ResetShoppingListUseCase()
 start_task_usecase = StartTaskUsecase(task_repository=task_repository)
 
@@ -30,12 +28,11 @@ if Environment.is_dev():
 def handler(event: dict, context: dict) -> None:
     try:
         logger.info("更新されたタスクに関して自動調整")
-        # FIXME: 前回のバッチ実行開始時刻にしたいけど、とりあえずこれで
-        latest_edited_at = jst_now() - timedelta(minutes=10)
-        maintain_tasks_usecase.execute(latest_edited_at)
+        maintain_tasks_usecase.execute()
 
         logger.info("タイトルが空のページを削除")
         clean_empty_title_page_usecase.handle()
+
         logger.info("買い物リストの購入ステータスをリセット")
         reset_shopping_list_usecase.execute()
 
--- notion_api/usecase/task/maintain_tasks_usecase.py
+++ notion_api/usecase/task/maintain_tasks_usecase.py
@@ -1,36 +1,47 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 from logging import Logger
 
+from lotion import Lotion
+
 from custom_logger import get_logger
-from task.task_repository import TaskRepository
+from notion_databases.task import Task, TaskStartDate, TaskStatus
+from notion_databases.task_prop.task_status import TaskStatusType
 from util.datetime import jst_now
 from util.line.line_client import LineClient
 
 
 class MaintainTasksUsecase:
-    def __init__(self, task_repository: TaskRepository, logger: Logger | None = None) -> None:
-        self._task_repository = task_repository
+    def __init__(self, logger: Logger | None = None) -> None:
         self._logger = logger or get_logger(__name__)
         self._line_client = LineClient.get_instance()
+        self._lotion = Lotion.get_instance(logger=self._logger)
 
-    def execute(self, last_edited_at: datetime) -> None:
-        tasks = self._task_repository.search(last_edited_at=last_edited_at)
+    def execute(self) -> None:
+        self._execute_scheduled()
+        self._execute_last_edited()
+
+    def _execute_scheduled(self) -> None:
+        now = jst_now()
+        start_prop = TaskStartDate.from_range(now)
+        end_prop = TaskStartDate.from_range(now + timedelta(minutes=3))
+        status_prop = TaskStatus.from_status_type(TaskStatusType.TODO)
+        tasks = self._lotion.search_pages(Task, [start_prop, end_prop, status_prop])
+        for task in tasks:
+            self._lotion.update(task.start())
+            self._line_client.push_message(f"タスク「{task.get_title_text()}」を開始しました")
+
+    def _execute_last_edited(self) -> None:
+        latest_edited_at = jst_now() - timedelta(minutes=10)
+        tasks = self._lotion.search_page_by_last_edited_at(Task, start=latest_edited_at)
 
         for task in tasks:
-            if task.status.is_todo() and task.is_scheduled():
-                self._logger.info(f"「予定」タスクを処理: {task.get_title_text()}")
-                self._task_repository.save(task=task.start())
-                self._line_client.push_message(f"タスク「{task.get_title_text()}」を開始しました")
             if task.is_completed and not task.get_title_text().startswith("✔️"):
                 self._logger.info(f"チェックマークをつける: {task.get_title_text()}")
-                self._task_repository.save(task=task.add_check_prefix())
+                self._lotion.update(task.add_check_prefix())
 
 
 if __name__ == "__main__":
     # python -m notion_api.usecase.task.maintain_tasks_usecase
-    from task.task_repository_impl import TaskRepositoryImpl
 
-    task_repository = TaskRepositoryImpl()
-    usecase = MaintainTasksUsecase(task_repository=task_repository)
-    last_edited_at = jst_now() - timedelta(minutes=30)
-    usecase.execute(last_edited_at=last_edited_at)
+    usecase = MaintainTasksUsecase()
+    usecase._execute_last_edited()
